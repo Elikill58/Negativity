0c6aca8814bd7be7b6db94cea1caf3757c5b0852
RedNesto <RedNestoMC@gmail.com>
2019-04-11 17:41:32 +0200

    Make NegativityAccount no longer abstract in order to instantiate it directly and because there is no reason to have it abstract for now

--- src/com/elikill58/negativity/universal/NegativityAccount.java
+++ src/com/elikill58/negativity/universal/NegativityAccount.java
@@ -6,15 +6,21 @@ import com.elikill58.negativity.universal.utils.NonnullByDefault;
  * Contains player-related data that can be accessed when the player is offline.
  */
 @NonnullByDefault
-public abstract class NegativityAccount {
+public class NegativityAccount {
 
 	private String lang = TranslatedMessages.DEFAULT_LANG;
+
+	public NegativityAccount() {}
+
+	public NegativityAccount(String lang) {
+		this.lang = lang;
+	}
 
 	public String getLang() {
 		return lang;
 	}
 
-	public void setLang(String s) {
-		this.lang = s;
+	public void setLang(String lang) {
+		this.lang = lang;
 	}
 }

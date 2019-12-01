f191793109daf09153381c9641bb2e1d10981981
RedNesto <RedNestoMC@gmail.com>
2019-12-01 23:07:40 +0100

    Update the Sponge plugin to catch up Spigot's one: Part 2

--- src/com/elikill58/negativity/sponge/protocols/SpeedHackProtocol.java
+++ src/com/elikill58/negativity/sponge/protocols/SpeedHackProtocol.java
@@ -1,11 +1,10 @@
 package com.elikill58.negativity.sponge.protocols;
 
+import org.spongepowered.api.block.BlockType;
 import org.spongepowered.api.block.BlockTypes;
 import org.spongepowered.api.data.key.Keys;
-import org.spongepowered.api.data.manipulator.mutable.PotionEffectData;
-import org.spongepowered.api.effect.potion.PotionEffect;
 import org.spongepowered.api.effect.potion.PotionEffectTypes;
-import org.spongepowered.api.entity.Transform;
+import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.entity.living.player.gamemode.GameModes;
 import org.spongepowered.api.event.Listener;
@@ -13,57 +12,86 @@ import org.spongepowered.api.event.entity.DamageEntityEvent;
 import org.spongepowered.api.event.entity.MoveEntityEvent;
 import org.spongepowered.api.event.filter.cause.First;
 import org.spongepowered.api.item.ItemTypes;
+import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 
 import com.elikill58.negativity.sponge.SpongeNegativity;
 import com.elikill58.negativity.sponge.SpongeNegativityPlayer;
 import com.elikill58.negativity.sponge.utils.Utils;
-import com.elikill58.negativity.universal.*;
+import com.elikill58.negativity.universal.Cheat;
+import com.elikill58.negativity.universal.NegativityPlayer;
+import com.elikill58.negativity.universal.ReportType;
 import com.flowpowered.math.vector.Vector3d;
+import com.flowpowered.math.vector.Vector3i;
 
 public class SpeedHackProtocol extends Cheat {
 
 	public SpeedHackProtocol() {
-		super("SPEEDHACK", false, ItemTypes.BEACON, true, true, "speed");
+		super("SPEEDHACK", false, ItemTypes.BEACON, true, true, "speed", "speedhack");
 	}
-	
+
 	@Listener
 	public void onPlayerMove(MoveEntityEvent e, @First Player p) {
-		if (!p.gameMode().get().equals(GameModes.SURVIVAL) && !p.gameMode().get().equals(GameModes.ADVENTURE))
+		if (!p.gameMode().get().equals(GameModes.SURVIVAL) && !p.gameMode().get().equals(GameModes.ADVENTURE)) {
 			return;
+		}
+
 		SpongeNegativityPlayer np = SpongeNegativityPlayer.getNegativityPlayer(p);
-		if (!np.hasDetectionActive(this))
+		if (!np.hasDetectionActive(this)) {
 			return;
-		Transform<World> from = e.getFromTransform(), to = e.getToTransform();
-		Vector3d toVect = to.getPosition(), fromVect = from.getPosition();
-		double y = toVect.clone().sub(0, toVect.getY(), 0).distance(fromVect.clone().sub(0, fromVect.getY(), 0));
-		if (p.getLocation().sub(0, 1, 0).getBlock().getType().equals(BlockTypes.SPONGE) || p.getVehicle() != null
-				|| np.isFlying() || from.getYaw() > to.getYaw() || p.get(Keys.WALKING_SPEED).get() > 2.0F
-				|| p.getOrCreate(PotionEffectData.class).get().contains(PotionEffect.of(PotionEffectTypes.SPEED, 1, 1)))
+		}
+
+		Vector3d fromVect = e.getFromTransform().getPosition();
+		Vector3d toVect = e.getToTransform().getPosition();
+		if (p.getLocation().sub(Vector3i.UNIT_Y).getBlockType().equals(BlockTypes.SPONGE)
+				|| np.isFlying() || fromVect.getY() > toVect.getY() || p.get(Keys.WALKING_SPEED).get() > 2.0F
+				|| np.hasPotionEffect(PotionEffectTypes.SPEED) || p.getVehicle().isPresent()) {
 			return;
+		}
+
 		if (np.BYPASS_SPEED != 0) {
 			np.BYPASS_SPEED--;
 			return;
 		}
+
+		if (canBoostWithPackedIce(p.getLocation())) {
+			return;
+		}
+
 		ReportType type = (np.getWarn(this) > 7) ? ReportType.VIOLATION : ReportType.WARNING;
 		boolean mayCancel = false;
-		String proof = "In ground: " + p.isOnGround() + "WalkSpeed: " + p.get(Keys.WALKING_SPEED) + "  Distance between from/to location: " + y;
-		if (p.isOnGround() && y >= 0.75D) {
-			SpongeNegativity.alertMod(type, p, this, Utils.parseInPorcent(y * 100 * 2), proof,
-					"Distance Last/New position: " + y + "\n(With same Y)\nPlayer on ground");
-		} else if (!p.isOnGround() && y >= 0.85D) {
-			mayCancel = SpongeNegativity.alertMod(type, p, this, Utils.parseInPorcent(y * 100 * 2), proof,
-					"Distance Last/New position: " + y + "\n(With same Y)\nPlayer jumping");
+		double distance = toVect.sub(0, toVect.getY(), 0).distance(fromVect.sub(0, fromVect.getY(), 0));
+		String proof = "In ground: " + p.isOnGround() + "WalkSpeed: " + p.get(Keys.WALKING_SPEED).get() + "  Distance between from/to location: " + distance;
+		if (p.isOnGround() && distance >= 0.75D) {
+			mayCancel = SpongeNegativity.alertMod(type, p, this, Utils.parseInPorcent(distance * 100 * 2), proof,
+					"Distance Last/New position: " + distance + "\n(With same Y)\nPlayer on ground");
+		} else if (!p.isOnGround() && distance >= 0.85D) {
+			mayCancel = SpongeNegativity.alertMod(type, p, this, Utils.parseInPorcent(distance * 100 * 2), proof,
+					"Distance Last/New position: " + distance + "\n(With same Y)\nPlayer jumping");
 		}
-		if (mayCancel && isSetBack())
+
+		if (mayCancel && isSetBack()) {
 			e.setCancelled(true);
+		}
+	}
+
+	private boolean canBoostWithPackedIce(Location<World> feetLocation) {
+		if (feetLocation.sub(Vector3i.UNIT_Y).getBlockType() != BlockTypes.PACKED_ICE) {
+			return false;
+		}
+
+		BlockType blockTypeAtHead = feetLocation.add(Vector3i.UNIT_Y).getBlockType();
+		return blockTypeAtHead == BlockTypes.TRAPDOOR || blockTypeAtHead == BlockTypes.IRON_TRAPDOOR;
 	}
 
 	@Listener
-	public void onEntityDamage(DamageEntityEvent e, @First Player p) {
-		SpongeNegativityPlayer.getNegativityPlayer(p).BYPASS_SPEED = 2;
+	public void onEntityDamage(DamageEntityEvent e) {
+		Entity damaged = e.getTargetEntity();
+		if (damaged instanceof Player) {
+			SpongeNegativityPlayer.getNegativityPlayer((Player) damaged).BYPASS_SPEED = 3;
+		}
 	}
-	
+
 	@Override
 	public String getHoverFor(NegativityPlayer p) {
 		return "";

2a34a541c90c2cc7ba5551550f57ba434333bc21
RedNesto <RedNestoMC@gmail.com>
2020-01-04 14:01:41 +0100

    Cleanup Spigot's Ban/Mod/UnbanCommand

--- src/com/elikill58/negativity/spigot/commands/ModCommand.java
+++ src/com/elikill58/negativity/spigot/commands/ModCommand.java
@@ -5,19 +5,18 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import com.elikill58.negativity.spigot.Messages;
 import com.elikill58.negativity.spigot.inventories.ModInventory;
-import com.elikill58.negativity.universal.TranslatedMessages;
 
 public class ModCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] arg) {
 		if(!(sender instanceof Player)) {
-			sender.sendMessage(TranslatedMessages.getStringFromLang(TranslatedMessages.DEFAULT_LANG, "only_player"));
+			Messages.sendMessage(sender, "only_player");
 			return false;
 		}
 		ModInventory.openModMenu((Player) sender);
 		return false;
 	}
-
 }

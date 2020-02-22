2815d4346730567b5193214ffb2fd787bfd2e7c2
Elikill58 <arpetzouille@gmail.com>
2020-02-22 11:43:24 +0100

    Fixed #61

--- src/com/elikill58/negativity/spigot/protocols/AirJumpProtocol.java
+++ src/com/elikill58/negativity/spigot/protocols/AirJumpProtocol.java
@@ -31,7 +31,7 @@ public class AirJumpProtocol extends Cheat implements Listener {
 		SpigotNegativityPlayer np = SpigotNegativityPlayer.getNegativityPlayer(p);
 		if (!np.ACTIVE_CHEAT.contains(this))
 			return;
-		if (p.isFlying() || p.getVehicle() != null || p.getItemInHand().getType().name().contains("TRIDENT"))
+		if (p.isFlying() || p.getVehicle() != null || p.getItemInHand().getType().name().contains("TRIDENT") || np.hasElytra())
 			return;
 		double temp = e.getTo().getY() - e.getFrom().getY();
 		Location loc = p.getLocation().clone();

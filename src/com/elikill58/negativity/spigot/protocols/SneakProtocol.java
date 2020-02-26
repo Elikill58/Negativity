38299a6dc2bf2c7e876483a856cfd956d0f8d823
Elikill58 <arpetzouille@gmail.com>
2020-02-26 22:22:50 +0100

    Update sneak detection

--- src/com/elikill58/negativity/spigot/protocols/SneakProtocol.java
+++ src/com/elikill58/negativity/spigot/protocols/SneakProtocol.java
@@ -1,15 +1,38 @@
 package com.elikill58.negativity.spigot.protocols;
 
+import org.bukkit.GameMode;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
 
+import com.elikill58.negativity.spigot.SpigotNegativity;
+import com.elikill58.negativity.spigot.SpigotNegativityPlayer;
 import com.elikill58.negativity.universal.Cheat;
 import com.elikill58.negativity.universal.CheatKeys;
 import com.elikill58.negativity.universal.NegativityPlayer;
+import com.elikill58.negativity.universal.ReportType;
 
-public class SneakProtocol extends Cheat {
+public class SneakProtocol extends Cheat implements Listener {
 
 	public SneakProtocol() {
-		super(CheatKeys.SNEAK, true, Material.BLAZE_POWDER, CheatCategory.MOVEMENT, false, "sneack");
+		super(CheatKeys.SNEAK, true, Material.BLAZE_POWDER, CheatCategory.MOVEMENT, true, "sneack");
+	}
+	
+	@EventHandler
+	public void onMove(PlayerMoveEvent e) {
+		Player p = e.getPlayer();
+		SpigotNegativityPlayer np = SpigotNegativityPlayer.getNegativityPlayer(p);
+		if (!np.ACTIVE_CHEAT.contains(this))
+			return;
+		if (!p.getGameMode().equals(GameMode.SURVIVAL) && !p.getGameMode().equals(GameMode.ADVENTURE))
+			return;
+		if (p.isSneaking() && p.isSprinting() && !p.isFlying()) {
+			boolean mayCancel = SpigotNegativity.alertMod(ReportType.WARNING, p, this, 99, "Sneaking, sprinting and not flying.", "", "");
+			if(mayCancel && isSetBack())
+				e.setCancelled(true);
+		}
 	}
 	
 	@Override
--- src/com/elikill58/negativity/sponge/protocols/SneakProtocol.java
+++ src/com/elikill58/negativity/sponge/protocols/SneakProtocol.java
@@ -1,17 +1,40 @@
 package com.elikill58.negativity.sponge.protocols;
 
+import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.entity.living.player.gamemode.GameModes;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.entity.MoveEntityEvent;
+import org.spongepowered.api.event.filter.cause.First;
 import org.spongepowered.api.item.ItemTypes;
 
+import com.elikill58.negativity.sponge.SpongeNegativity;
+import com.elikill58.negativity.sponge.SpongeNegativityPlayer;
 import com.elikill58.negativity.universal.Cheat;
 import com.elikill58.negativity.universal.CheatKeys;
 import com.elikill58.negativity.universal.NegativityPlayer;
+import com.elikill58.negativity.universal.ReportType;
 
 public class SneakProtocol extends Cheat {
 
 	public SneakProtocol() {
-		super(CheatKeys.SNEAK, true, ItemTypes.BLAZE_POWDER, CheatCategory.MOVEMENT, false, "sneack");
+		super(CheatKeys.SNEAK, true, ItemTypes.BLAZE_POWDER, CheatCategory.MOVEMENT, true, "sneack");
 	}
 
+	@Listener
+	public void onPlayerMove(MoveEntityEvent e, @First Player p) {
+		if (!p.gameMode().get().equals(GameModes.SURVIVAL) && !p.gameMode().get().equals(GameModes.ADVENTURE)) {
+			return;
+		}
+		SpongeNegativityPlayer np = SpongeNegativityPlayer.getNegativityPlayer(p);
+		if (!np.hasDetectionActive(this))
+			return;
+		if (p.get(Keys.IS_SNEAKING).orElse(false) && p.get(Keys.IS_SPRINTING).orElse(false) && !p.get(Keys.IS_FLYING).orElse(false)) {
+			boolean mayCancel = SpongeNegativity.alertMod(ReportType.WARNING, p, this, 99, "Sneaking, sprinting and not flying.", "", "");
+			if(mayCancel && isSetBack())
+				e.setCancelled(true);
+		}
+	}
 	
 	@Override
 	public String getHoverFor(NegativityPlayer p) {

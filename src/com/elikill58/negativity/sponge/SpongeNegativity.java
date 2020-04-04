188b4b285034fa61608479c38153ed3d3e1d07aa
Elikill58 <arpetzouille@gmail.com>
2020-04-04 15:43:01 +0200

    Upgrading to 1.6

--- src/com/elikill58/negativity/sponge/SpongeNegativity.java
+++ src/com/elikill58/negativity/sponge/SpongeNegativity.java
@@ -103,7 +103,7 @@ import com.google.inject.Inject;
 import ninja.leaping.configurate.ConfigurationNode;
 import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
 
-@Plugin(id = "negativity", name = "Negativity", version = "1.5.2", description = "It's an Advanced AntiCheat Detection", authors = "Elikill58", dependencies = {
+@Plugin(id = "negativity", name = "Negativity", version = "1.6", description = "It's an Advanced AntiCheat Detection", authors = { "Elikill58", "RedNesto" }, dependencies = {
 		@Dependency(id = "packetgate") })
 public class SpongeNegativity {
 

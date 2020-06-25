873ac4b948fd3ef49384327758875adf57dbde8e
RedNesto <RedNestoMC@gmail.com>
2020-06-25 11:29:50 +0200

    Cheats column must be a LONGTEXT

--- src/databaseMigrations/verifications/000-Create-Initial-Table.sql
+++ src/databaseMigrations/verifications/000-Create-Initial-Table.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS negativity_verifications
     uuid CHAR(36) NOT NULL PRIMARY KEY,
     startedBy VARCHAR(16),
     result VARCHAR(1024) NOT NULL,
-    cheats VARCHAR(1024) NOT NULL,
+    cheats LONGTEXT NOT NULL,
     player_version VARCHAR(16) NOT NULL,
     version INT(11) NOT NULL,
     creation_time TIMESTAMP NOT NULL DEFAULT NOW()

5aee75b23eea56d0bd8d0e9a3003970556cedccb
Elikill58 <arpetzouille@gmail.com>
2020-08-16 17:36:42 +0200

    Update spider detections

--- src/com/elikill58/negativity/common/protocols/Spider.java
+++ src/com/elikill58/negativity/common/protocols/Spider.java
@@ -48,7 +48,7 @@ public class Spider extends Cheat implements Listeners {
 			return;
 		double y = e.getTo().getY() - e.getFrom().getY();
 		boolean isAris = ((float) y) == p.getWalkSpeed();
-		if (((y > 0.499 && y < 0.7) || isAris) && !np.isUsingSlimeBlock && !p.isSprinting()) {
+		if (((y > 0.499 && y < 0.7) || isAris) && !np.isUsingSlimeBlock && !p.isSprinting() && p.getVelocity().length() < 1.5) {
 			int relia = UniversalUtils.parseInPorcent(y * 160 + (isAris ? 39 : 0));
 			if (Negativity.alertMod((np.getWarn(this) > 6 ? ReportType.WARNING : ReportType.VIOLATION), p, this,
 					relia, "Nothing around him. To > From: " + y + " isAris: " + isAris + ", has not stab slairs")

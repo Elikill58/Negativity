dceedf4ae3c0bb575844b3f8a9a5b4b097656ea3
Elikill58 <arpetzouille@gmail.com>
2020-08-20 14:41:47 +0200

    Edit InventoryActions

--- src/com/elikill58/negativity/spigot/listeners/InventoryListeners.java
+++ src/com/elikill58/negativity/spigot/listeners/InventoryListeners.java
@@ -3,6 +3,7 @@ package com.elikill58.negativity.spigot.listeners;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 
 import com.elikill58.negativity.api.events.EventManager;
@@ -28,11 +29,40 @@ public class InventoryListeners implements Listener {
 		if(!(e.getWhoClicked() instanceof Player) || e.getClickedInventory() == null || e.getCurrentItem() == null)
 			return;
 		com.elikill58.negativity.api.entity.Player p = SpigotEntityManager.getPlayer((Player) e.getWhoClicked());
-		InventoryAction action = InventoryAction.valueOf(e.getAction().name());
+		InventoryAction action = getAction(e.getClick());
 		ItemStack item = new SpigotItemStack(e.getCurrentItem());
 		InventoryClickEvent event = new InventoryClickEvent(p, action, e.getSlot(), item, new SpigotInventory(e.getClickedInventory()));
 		EventManager.callEvent(event);
 		e.setCancelled(event.isCancelled());
+	}
+	
+	private InventoryAction getAction(ClickType type) {
+		switch (type) {
+		case CONTROL_DROP:
+		case DROP:
+			return InventoryAction.DROP;
+		case CREATIVE:
+			return InventoryAction.CREATIVE;
+		case DOUBLE_CLICK:
+			return InventoryAction.DOUBLE;
+		case LEFT:
+		case WINDOW_BORDER_LEFT:
+			return InventoryAction.LEFT;
+		case SHIFT_LEFT:
+			return InventoryAction.LEFT_SHIFT;
+		case MIDDLE:
+			return InventoryAction.MIDDLE;
+		case RIGHT:
+		case WINDOW_BORDER_RIGHT:
+			return InventoryAction.RIGHT;
+		case SHIFT_RIGHT:
+			return InventoryAction.RIGHT_SHIFT;
+		case NUMBER_KEY:
+			return InventoryAction.NUMBER;
+		case UNKNOWN:
+			return InventoryAction.UNKNOWN;
+		}
+		return InventoryAction.UNKNOWN;
 	}
 	
 	@EventHandler
--- src/com/elikill58/negativity/sponge/listeners/InventoryListeners.java
+++ src/com/elikill58/negativity/sponge/listeners/InventoryListeners.java
@@ -3,7 +3,6 @@ package com.elikill58.negativity.sponge.listeners;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.filter.cause.First;
-import org.spongepowered.api.event.filter.type.Exclude;
 import org.spongepowered.api.event.item.inventory.ClickInventoryEvent;
 import org.spongepowered.api.event.item.inventory.InteractInventoryEvent;
 
@@ -27,15 +26,30 @@ public class InventoryListeners {
 	}
 	
 	@Listener
-	@Exclude(ClickInventoryEvent.Double.class)
 	public void onInvClick(ClickInventoryEvent e, @First Player p) {
 		if(e.getTargetInventory() == null)
 			return;
-		InventoryAction action = InventoryAction.valueOf("");
+		InventoryAction action = getAction(e);
 		ItemStack item = new SpongeItemStack(e.getTransactions().get(0).getOriginal().createStack());
 		InventoryClickEvent event = new InventoryClickEvent(SpongeEntityManager.getPlayer(p), action, -1, item, new SpongeInventory(e.getTargetInventory()));
 		EventManager.callEvent(event);
 		e.setCancelled(event.isCancelled());
+	}
+	
+	private InventoryAction getAction(ClickInventoryEvent e) {
+		if(e instanceof ClickInventoryEvent.Double)
+			return InventoryAction.DOUBLE;
+		else if(e instanceof ClickInventoryEvent.Creative)
+			return InventoryAction.CREATIVE;
+		else if(e instanceof ClickInventoryEvent.Drop)
+			return InventoryAction.DROP;
+		else if(e instanceof ClickInventoryEvent.Middle)
+			return InventoryAction.MIDDLE;
+		else if(e instanceof ClickInventoryEvent.Shift)
+			return InventoryAction.LEFT_SHIFT;
+		else if(e instanceof ClickInventoryEvent.NumberPress)
+			return InventoryAction.NUMBER;
+		return InventoryAction.UNKNOWN;
 	}
 	
 	@Listener

ba41d23cdb3d21c500622b6470968f4854bdbf92
Elikill58 <arpetzouille@gmail.com>
2020-08-23 17:21:58 +0200

    Fix false flag while using elytra

--- src/com/elikill58/negativity/spigot/protocols/SpeedProtocol.java
+++ src/com/elikill58/negativity/spigot/protocols/SpeedProtocol.java
@@ -16,6 +16,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityToggleGlideEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.potion.PotionEffectType;
 
@@ -132,6 +133,12 @@ public class SpeedProtocol extends Cheat implements Listener {
 			if (et.getType().equals(EntityType.ENDER_DRAGON) && et.getLocation().distance(p.getLocation()) < 15)
 				return true;
 		return false;
+	}
+	
+	@EventHandler
+	public void onGlide(EntityToggleGlideEvent e) {
+		if(!e.isGliding() && e.getEntity() instanceof Player)
+			SpigotNegativityPlayer.getNegativityPlayer((Player) e.getEntity()).TIME_INVINCIBILITY = System.currentTimeMillis() + 1000;
 	}
 
 	@EventHandler

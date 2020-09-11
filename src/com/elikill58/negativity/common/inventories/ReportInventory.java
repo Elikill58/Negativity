b99d1d688ba7e59418d46bf905abc4b92a9c6c86
Elikill58 <arpetzouille@gmail.com>
2020-09-11 20:28:05 +0200

    Add pages to report inventory

--- src/com/elikill58/negativity/common/inventories/ReportInventory.java
+++ src/com/elikill58/negativity/common/inventories/ReportInventory.java
@@ -32,24 +32,37 @@ public class ReportInventory extends AbstractInventory {
 		Player cible = (Player) args[0];
 		NegativityAccount na = NegativityAccount.get(cible.getUniqueId());
 		List<Report> reports = na.getReports();
-		Inventory inv = Inventory.createInventory("Reports", UniversalUtils.getMultipleOf(reports.size(), 9, 1, 54), new ReportHolder(cible));
+		int offset = (args.length == 1 ? 0 : (int) args[1]);
+		if(offset < 0)
+			offset = 0;
+		Inventory inv = Inventory.createInventory("Reports", UniversalUtils.getMultipleOf(reports.size(), 9, 1, 54), new ReportHolder(cible, offset));
 		for(int i = 0; i < 9; i++) inv.set(i, ItemBuilder.Builder(Materials.GRAY_STAINED_GLASS_PANE).build());
 		inv.set(3, ItemBuilder.getSkullItem(cible));
-		if(reports.size() > 45) {
-			// there is pages
-			
-		} else {
-			for(int i = 9; i < reports.size(); i++) {
-				Report r = reports.get(i - 9);
-				inv.set(i, ItemBuilder.Builder(Materials.APPLE).displayName(Adapter.getAdapter().getOfflinePlayer(r.getReportedBy()).getName())
-						.lore(r.getReason()).build());
-			}
+		int max = reports.size() + offset;
+		if(max > 45)
+			max = 45;
+		int slot = 9;
+		int i;
+		for(i = offset; i < max; i++) {
+			Report r = reports.get(i);
+			inv.set(slot++, ItemBuilder.Builder(Materials.APPLE).displayName(Adapter.getAdapter().getOfflinePlayer(r.getReportedBy()).getName())
+					.lore(r.getReason()).build());
 		}
+		if(offset > 0)
+			inv.set(2, ItemBuilder.Builder(Materials.ARROW).displayName("Last").build());
+		if(i == 45)
+			inv.set(5, ItemBuilder.Builder(Materials.ARROW).displayName("Next").build());
 		p.openInventory(inv);
 	}
 
 	@Override
 	public void manageInventory(InventoryClickEvent e, Material m, Player p, NegativityHolder nh) {
-		
+		ReportHolder rh = (ReportHolder) nh;
+		if(m.equals(Materials.ARROW)) {
+			if(e.getSlot() < 3)
+				openInventory(p, rh.getCible(), rh.getOffset() - 45);
+			else
+				openInventory(p, rh.getCible(), rh.getOffset() + 45);
+		}
 	}
 }
--- src/com/elikill58/negativity/common/inventories/holders/ReportHolder.java
+++ src/com/elikill58/negativity/common/inventories/holders/ReportHolder.java
@@ -6,12 +6,18 @@ import com.elikill58.negativity.api.inventory.NegativityHolder;
 public class ReportHolder extends NegativityHolder {
 	
 	private final Player cible;
+	private final int offset;
 	
-	public ReportHolder(Player cible) {
+	public ReportHolder(Player cible, int offset) {
 		this.cible = cible;
+		this.offset = offset;
 	}
 
 	public Player getCible() {
 		return cible;
+	}
+	
+	public int getOffset() {
+		return offset;
 	}
 }

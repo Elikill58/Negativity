ad6c8a2c4f1785861217a76e3877a161058f4640
Elikill58 <arpetzouille@gmail.com>
2020-09-11 20:37:29 +0200

    FIx offset report inv

--- src/com/elikill58/negativity/common/inventories/ReportInventory.java
+++ src/com/elikill58/negativity/common/inventories/ReportInventory.java
@@ -6,11 +6,13 @@ import com.elikill58.negativity.api.entity.Player;
 import com.elikill58.negativity.api.events.inventory.InventoryClickEvent;
 import com.elikill58.negativity.api.inventory.AbstractInventory;
 import com.elikill58.negativity.api.inventory.Inventory;
+import com.elikill58.negativity.api.inventory.InventoryManager;
 import com.elikill58.negativity.api.inventory.NegativityHolder;
 import com.elikill58.negativity.api.item.ItemBuilder;
 import com.elikill58.negativity.api.item.Material;
 import com.elikill58.negativity.api.item.Materials;
 import com.elikill58.negativity.common.inventories.holders.ReportHolder;
+import com.elikill58.negativity.universal.Messages;
 import com.elikill58.negativity.universal.NegativityAccount;
 import com.elikill58.negativity.universal.Report;
 import com.elikill58.negativity.universal.adapter.Adapter;
@@ -37,10 +39,10 @@ public class ReportInventory extends AbstractInventory {
 			offset = 0;
 		Inventory inv = Inventory.createInventory("Reports", UniversalUtils.getMultipleOf(reports.size(), 9, 1, 54), new ReportHolder(cible, offset));
 		for(int i = 0; i < 9; i++) inv.set(i, ItemBuilder.Builder(Materials.GRAY_STAINED_GLASS_PANE).build());
+		inv.set(0, ItemBuilder.Builder(Materials.ARROW).displayName(Messages.getMessage(p, "inventory.back")).build());
 		inv.set(3, ItemBuilder.getSkullItem(cible));
-		int max = reports.size() + offset;
-		if(max > 45)
-			max = 45;
+		inv.set(8, ItemBuilder.Builder(Materials.BARRIER).displayName(Messages.getMessage(p, "inventory.close")).build());
+		int max = reports.size() > 45 ? 45 : reports.size() + offset;
 		int slot = 9;
 		int i;
 		for(i = offset; i < max; i++) {
@@ -50,7 +52,7 @@ public class ReportInventory extends AbstractInventory {
 		}
 		if(offset > 0)
 			inv.set(2, ItemBuilder.Builder(Materials.ARROW).displayName("Last").build());
-		if(i == 45)
+		if(slot == 45)
 			inv.set(5, ItemBuilder.Builder(Materials.ARROW).displayName("Next").build());
 		p.openInventory(inv);
 	}
@@ -59,7 +61,10 @@ public class ReportInventory extends AbstractInventory {
 	public void manageInventory(InventoryClickEvent e, Material m, Player p, NegativityHolder nh) {
 		ReportHolder rh = (ReportHolder) nh;
 		if(m.equals(Materials.ARROW)) {
-			if(e.getSlot() < 3)
+			int slot = e.getSlot();
+			if(slot == 0)
+				InventoryManager.open(NegativityInventory.CHEAT_MANAGER, p, rh.getCible());
+			else if(e.getSlot() == 2)
 				openInventory(p, rh.getCible(), rh.getOffset() - 45);
 			else
 				openInventory(p, rh.getCible(), rh.getOffset() + 45);

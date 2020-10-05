e2c447e60c7f3bffacea4212c116ef12257d5969
Elikill58 <arpetzouille@gmail.com>
2020-10-05 14:51:31 +0200

    Add FastBow to verif

--- src/com/elikill58/negativity/common/protocols/FastBow.java
+++ src/com/elikill58/negativity/common/protocols/FastBow.java
@@ -3,6 +3,7 @@ package com.elikill58.negativity.common.protocols;
 import static com.elikill58.negativity.universal.CheatKeys.FAST_BOW;
 
 import com.elikill58.negativity.api.NegativityPlayer;
+import com.elikill58.negativity.api.colors.ChatColor;
 import com.elikill58.negativity.api.entity.EntityType;
 import com.elikill58.negativity.api.entity.Player;
 import com.elikill58.negativity.api.events.EventListener;
@@ -17,8 +18,13 @@ import com.elikill58.negativity.universal.Negativity;
 import com.elikill58.negativity.universal.ReportType;
 import com.elikill58.negativity.universal.bypass.checkers.ItemUseBypass;
 import com.elikill58.negativity.universal.utils.UniversalUtils;
+import com.elikill58.negativity.universal.verif.VerifData;
+import com.elikill58.negativity.universal.verif.VerifData.DataType;
+import com.elikill58.negativity.universal.verif.data.LongDataCounter;
 
 public class FastBow extends Cheat implements Listeners {
+
+	public static final DataType<Long> TIME_SHOT = new DataType<Long>("time_shot", "Time between shot", () -> new LongDataCounter());
 	
 	public FastBow() {
 		super(FAST_BOW, true, Materials.BOW, CheatCategory.COMBAT, true, "bow");
@@ -34,12 +40,13 @@ public class FastBow extends Cheat implements Listeners {
 		if(!np.hasDetectionActive(this) || !checkActive("last-shot"))
 			return;
 
-		if(ItemUseBypass.hasBypassWithClick(p, this, item, e.getAction().name()))
-			return;
 		if (item.getType().equals(Materials.BOW) && e.getAction().name().contains("RIGHT_CLICK")) {
+			if(ItemUseBypass.hasBypassWithClick(p, this, item, e.getAction().name()))
+				return;
 			np.flyingReason = FlyingReason.BOW;
 			long lastShotWithBow = np.longs.get(FAST_BOW, "last-shot", 0l);
 			long actual = System.currentTimeMillis(), dif = actual - lastShotWithBow;
+			recordData(p.getUniqueId(), TIME_SHOT, dif);
 			if (lastShotWithBow != 0) {
 				int ping = p.getPing();
 				if (dif < (200 + ping)) {
@@ -64,5 +71,10 @@ public class FastBow extends Cheat implements Listeners {
 	public void onShot(EntityShootBowEvent e){
 		if(e.getEntity().getType().equals(EntityType.PLAYER))
 			NegativityPlayer.getNegativityPlayer((Player) e.getEntity()).flyingReason = FlyingReason.BOW;
+	}
+	
+	@Override
+	public String makeVerificationSummary(VerifData data, NegativityPlayer np) {
+		return "Time between shot : " + ChatColor.GREEN + data.getData(TIME_SHOT).getAverage();
 	}
 }

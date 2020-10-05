90841e479d786b3c1ebe11fbf61af5e4ecd8f699
Elikill58 <arpetzouille@gmail.com>
2020-10-05 14:20:11 +0200

    Add scaffold check

--- src/com/elikill58/negativity/common/protocols/Scaffold.java
+++ src/com/elikill58/negativity/common/protocols/Scaffold.java
@@ -1,8 +1,10 @@
 package com.elikill58.negativity.common.protocols;
 
+import java.util.List;
 import java.util.TimerTask;
 
 import com.elikill58.negativity.api.NegativityPlayer;
+import com.elikill58.negativity.api.block.Block;
 import com.elikill58.negativity.api.entity.Player;
 import com.elikill58.negativity.api.events.EventListener;
 import com.elikill58.negativity.api.events.Listeners;
@@ -31,33 +33,44 @@ public class Scaffold extends Cheat implements Listeners {
 		NegativityPlayer np = NegativityPlayer.getNegativityPlayer(p);
 		if (!np.hasDetectionActive(this))
 			return;
-		if(!checkActive("below"))
-			return;
-		int ping = p.getPing(), slot = p.getInventory().getHeldItemSlot();
-		if (ping > 120)
-			return;
-		new java.util.Timer().schedule(new TimerTask() {
-			
-			@Override
-			public void run() {
-				Material m = p.getItemInHand().getType(), placed = e.getBlock().getType();
-				if ((m == null || (!m.isSolid() && !m.equals(placed))) && slot != p.getInventory().getHeldItemSlot()
-						&& !placed.equals(Materials.AIR)) {
-					int localPing = ping;
-					if (localPing == 0)
-						localPing = 1;
-					boolean mayCancel = Negativity.alertMod(ReportType.WARNING, p, Scaffold.this,
-							UniversalUtils.parseInPorcent(120 / localPing), "below",
-							"Item in hand: " + m.getId() + " Block placed: " + placed.getId(),
-							hoverMsg("main", "%item%", m.getId().toLowerCase(), "%block%",
-									placed.getId().toLowerCase()));
-					if (isSetBack() && mayCancel) {
-						p.getInventory().addItem(ItemBuilder.Builder(placed).build());
-						e.getBlock().setType(Materials.AIR);
+		if(checkActive("below")) {
+			int ping = p.getPing(), slot = p.getInventory().getHeldItemSlot();
+			if (ping > 120)
+				return;
+			new java.util.Timer().schedule(new TimerTask() {
+				
+				@Override
+				public void run() {
+					Material m = p.getItemInHand().getType(), placed = e.getBlock().getType();
+					if ((m == null || (!m.isSolid() && !m.equals(placed))) && slot != p.getInventory().getHeldItemSlot()
+							&& !placed.equals(Materials.AIR)) {
+						int localPing = ping;
+						if (localPing == 0)
+							localPing = 1;
+						boolean mayCancel = Negativity.alertMod(ReportType.WARNING, p, Scaffold.this,
+								UniversalUtils.parseInPorcent(120 / localPing), "below",
+								"Item in hand: " + m.getId() + " Block placed: " + placed.getId(),
+								hoverMsg("main", "%item%", m.getId().toLowerCase(), "%block%",
+										placed.getId().toLowerCase()));
+						if (isSetBack() && mayCancel) {
+							p.getInventory().addItem(ItemBuilder.Builder(placed).build());
+							e.getBlock().setType(Materials.AIR);
+						}
 					}
 				}
+			}, 50);
+		}
+		if(checkActive("distance")) {
+			Block place = e.getBlock();
+			List<Block> targetVisual = p.getTargetBlock(8);
+			if(place == null || targetVisual.isEmpty())
+				return;
+			Block nearTargetVisual = targetVisual.stream().sorted((b1, b2) -> (int) b1.getLocation().distance(b2.getLocation())).findFirst().orElse(null);
+			double distance = place.getLocation().distance(nearTargetVisual.getLocation());
+			if(distance > 3) {
+				Negativity.alertMod(distance > 5 ? ReportType.VIOLATION : ReportType.WARNING, p, this, UniversalUtils.parseInPorcent(distance * 30), "distance", "Place: " + place + ", targetVisual: " + targetVisual + ". Distance: " + distance);
 			}
-		}, 50);
+		}
 	}
 	
 	@EventListener

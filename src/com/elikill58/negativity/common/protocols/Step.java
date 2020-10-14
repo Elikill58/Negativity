e2817aab6af3caa48bb0d70737e33c792f82393c
Elikill58 <arpetzouille@gmail.com>
2020-10-14 22:49:45 +0200

    Fix step FP with carpet

--- src/com/elikill58/negativity/common/protocols/Step.java
+++ src/com/elikill58/negativity/common/protocols/Step.java
@@ -60,7 +60,7 @@ public class Step extends Cheat implements Listeners {
 			double diffBoost = dif - (amplifier / 10);
 			if(diffBoost > 0.2) {
 				recordData(p.getUniqueId(), BLOCKS_UP, diffBoost);
-				if(diffBoost > 0.5 || diffBoost == 0.25) {
+				if((diffBoost > 0.5 || diffBoost == 0.25) && diffBoost != 0.56) { // 0.56 is to bypass carpet
 					Negativity.alertMod(ReportType.WARNING, p, this, UniversalUtils.parseInPorcent(diffBoost == 0.25 ? 95 : diffBoost * 125), "dif-boost",
 							"Basic Y diff: " + dif + ", with boost: " + diffBoost + " (because of boost amplifier " + amplifier + ")",
 							hoverMsg("main", "%block%", String.format("%.2f", dif)), (int) ((diffBoost - 0.6) / 0.2));

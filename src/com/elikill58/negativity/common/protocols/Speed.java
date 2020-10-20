0fdaca03b39a9c68b51c48e36191fd3a1bfbfddd
Elikill58 <arpetzouille@gmail.com>
2020-10-20 23:23:31 +0200

    Fix #199

--- src/com/elikill58/negativity/common/protocols/Speed.java
+++ src/com/elikill58/negativity/common/protocols/Speed.java
@@ -137,7 +137,7 @@ public class Speed extends Cheat implements Listeners {
 		}
 		if(checkActive("walk-speed") && Adapter.getAdapter().getPlatformID().equals(Platform.SPIGOT)) {
 			double distanceWithSpeed = distance - (amplifierSpeed / 10);
-			if(dif == 0 && distanceWithSpeed >= p.getWalkSpeed() * 2) {
+			if(dif == 0 && distanceWithSpeed >= (p.getWalkSpeed() * (p.isSprinting() ? 2.5 : 2))) {
 				mayCancel = Negativity.alertMod(np.getWarn(this) > 7 ? ReportType.VIOLATION : ReportType.WARNING, p,
 						this, 95, "walk-speed", "Differences : " + dif + ", distance: " + String.format("%.4f", distance) + ", withSpeed: "
 						+ String.format("%.4f", distanceWithSpeed) + ", speedAmplifier: " + amplifierSpeed

c2eae568977be0b1117b5c69d3a6821126e9e124
RedNesto <RedNestoMC@gmail.com>
2020-11-25 18:45:52 +0100

    Fix Sponge InventoryClickEvent args

--- sponge/src/com/elikill58/negativity/sponge/listeners/InventoryListeners.java
+++ sponge/src/com/elikill58/negativity/sponge/listeners/InventoryListeners.java
@@ -5,6 +5,8 @@ import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.filter.cause.First;
 import org.spongepowered.api.event.item.inventory.ClickInventoryEvent;
 import org.spongepowered.api.event.item.inventory.InteractInventoryEvent;
+import org.spongepowered.api.item.inventory.property.SlotIndex;
+import org.spongepowered.api.item.inventory.transaction.SlotTransaction;
 
 import com.elikill58.negativity.api.events.EventManager;
 import com.elikill58.negativity.api.events.inventory.InventoryAction;
@@ -27,11 +29,13 @@ public class InventoryListeners {
 	
 	@Listener
 	public void onInvClick(ClickInventoryEvent e, @First Player p) {
-		if(e.getTargetInventory() == null || e.getTransactions().size() == 0)
+		if (e.getTransactions().isEmpty())
 			return;
 		InventoryAction action = getAction(e);
-		ItemStack item = new SpongeItemStack(e.getTransactions().get(0).getOriginal().createStack());
-		InventoryClickEvent event = new InventoryClickEvent(SpongeEntityManager.getPlayer(p), action, -1, item, new SpongeInventory(e.getTargetInventory()));
+		SlotTransaction transaction = e.getTransactions().get(0);
+		ItemStack item = new SpongeItemStack(transaction.getOriginal().createStack());
+		int slotIndex = transaction.getSlot().getInventoryProperty(SlotIndex.class).map(SlotIndex::getValue).orElse(-1);
+		InventoryClickEvent event = new InventoryClickEvent(SpongeEntityManager.getPlayer(p), action, slotIndex, item, new SpongeInventory(e.getTargetInventory()));
 		EventManager.callEvent(event);
 		e.setCancelled(event.isCancelled());
 	}
@@ -45,10 +49,17 @@ public class InventoryListeners {
 			return InventoryAction.DROP;
 		else if(e instanceof ClickInventoryEvent.Middle)
 			return InventoryAction.MIDDLE;
-		else if(e instanceof ClickInventoryEvent.Shift)
+		else if(e instanceof ClickInventoryEvent.Shift.Primary)
 			return InventoryAction.LEFT_SHIFT;
+		else if(e instanceof ClickInventoryEvent.Shift.Secondary)
+			return InventoryAction.RIGHT_SHIFT;
 		else if(e instanceof ClickInventoryEvent.NumberPress)
 			return InventoryAction.NUMBER;
+		// Order matters! Primary and Secondary are at the end because other events may subclass them (e.g. Shift.Primary)
+		else if(e instanceof ClickInventoryEvent.Primary)
+			return InventoryAction.LEFT;
+		else if(e instanceof ClickInventoryEvent.Secondary)
+			return InventoryAction.RIGHT;
 		return InventoryAction.UNKNOWN;
 	}
 	

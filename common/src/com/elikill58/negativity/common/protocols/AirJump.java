b5279be94bb7cc7503b747e0c8afe2b471f3d0b1
Elikill58 <arpetzouille@gmail.com>
2021-03-26 23:13:13 +0100

    Fix AirJump too

--- common/src/com/elikill58/negativity/common/protocols/AirJump.java
+++ common/src/com/elikill58/negativity/common/protocols/AirJump.java
@@ -1,5 +1,6 @@
 package com.elikill58.negativity.common.protocols;
 
+import static com.elikill58.negativity.api.utils.LocationUtils.hasOtherThan;
 import static com.elikill58.negativity.api.utils.LocationUtils.hasOtherThanExtended;
 import static com.elikill58.negativity.universal.CheatKeys.AIR_JUMP;
 
@@ -14,6 +15,7 @@ import com.elikill58.negativity.api.location.Location;
 import com.elikill58.negativity.api.utils.LocationUtils;
 import com.elikill58.negativity.universal.Cheat;
 import com.elikill58.negativity.universal.Negativity;
+import com.elikill58.negativity.universal.Scheduler;
 import com.elikill58.negativity.universal.report.ReportType;
 import com.elikill58.negativity.universal.utils.UniversalUtils;
 
@@ -31,36 +33,49 @@ public class AirJump extends Cheat implements Listeners {
 		NegativityPlayer np = NegativityPlayer.getNegativityPlayer(p);
 		if (!np.hasDetectionActive(this))
 			return;
-		if (p.isFlying() || p.isInsideVehicle() || p.getItemInHand().getType().getId().contains("TRIDENT") || p.hasElytra() || np.isInFight)
+		if (p.isFlying() || p.isInsideVehicle() || p.getItemInHand().getType().getId().contains("TRIDENT")
+				|| p.hasElytra() || np.isInFight)
 			return;
-		Location loc = p.getLocation().clone(), locDown = loc.clone().sub(0, 1, 0);
-		boolean mayCancel = false, hasOtherThanAir = hasOtherThanExtended(loc, "AIR"), hasOtherThanAirDown = hasOtherThanExtended(locDown, "AIR");
+		Location loc = p.getLocation().clone(), locDown = loc.clone().sub(0, 1, 0),
+				locDownDown = locDown.clone().sub(0, 1, 0);
 
-		double diffYtoFrom = e.getTo().getY() - e.getFrom().getY();
-		double lastDiffY = np.doubles.get(AIR_JUMP, "diff-y", 0.0);
-		if(checkActive("diff-y") && !np.booleans.get("ALL", "jump-boost-use", false)) {
-			if (diffYtoFrom > 0.35 && lastDiffY < diffYtoFrom && lastDiffY > 0 && !hasOtherThanAir
-					&& !hasOtherThanAirDown
-					&& !hasOtherThanExtended(loc.clone().sub(0, 2, 0), "AIR")) {
-				mayCancel = Negativity.alertMod(
-						diffYtoFrom > 0.5 && np.getWarn(this) > 5 ? ReportType.VIOLATION : ReportType.WARNING, p, this,
-								UniversalUtils.parseInPorcent((int) (diffYtoFrom * 210) - p.getPing()), "diff-y",
-						"Actual diff Y: " + lastDiffY + ", last diff Y: " + diffYtoFrom);
+		Scheduler.getInstance().runDelayed(() -> {
+			if (hasOtherThanExtended(loc, "AIR") || hasOtherThan(locDown, "AIR") || hasOtherThan(locDownDown, "AIR"))
+				return;
+			if (locDownDown.getBlock().getType().getId().contains("STAIR")
+					|| locDown.getBlock().getType().getId().contains("STAIR"))
+				return;
+			boolean mayCancel = false;
+
+			double diffYtoFrom = e.getTo().getY() - e.getFrom().getY();
+			double lastDiffY = np.doubles.get(AIR_JUMP, "diff-y", 0.0);
+			if (checkActive("diff-y") && !np.booleans.get("ALL", "jump-boost-use", false)) {
+				if (diffYtoFrom > 0.35 && lastDiffY < diffYtoFrom && lastDiffY > 0
+						&& !hasOtherThanExtended(loc.clone().sub(0, 2, 0), "AIR")) {
+					mayCancel = Negativity.alertMod(
+							diffYtoFrom > 0.5 && np.getWarn(this) > 5 ? ReportType.VIOLATION : ReportType.WARNING, p,
+							this, UniversalUtils.parseInPorcent((int) (diffYtoFrom * 210) - p.getPing()), "diff-y",
+							"Actual diff Y: " + lastDiffY + ", last diff Y: " + diffYtoFrom + ". Down: "
+									+ locDown.getBlock().getType().getId() + ", Down Down: "
+									+ locDownDown.getBlock().getType().getId());
+				}
+				lastDiffY = diffYtoFrom;
 			}
-			lastDiffY = diffYtoFrom;
-		}
-		
-		if(checkActive("going-down")) {
-			boolean wasGoingDown = np.booleans.get(AIR_JUMP, "going-down", false);
-			if(diffYtoFrom > lastDiffY && wasGoingDown && diffYtoFrom != 0.5) { // 0.5 when use stairs or slab
-				if(!hasOtherThanAirDown && !hasOtherThanAir && locDown.clone().sub(0, 1, 0).getBlock().getType().getId().contains("STAIRS")) {
-					mayCancel = Negativity.alertMod(ReportType.WARNING, p, this, UniversalUtils.parseInPorcent(diffYtoFrom * 200), "going-down",  "Was going down, last y " + lastDiffY + ", current: " + diffYtoFrom);
+
+			if (checkActive("going-down")) {
+				boolean wasGoingDown = np.booleans.get(AIR_JUMP, "going-down", false);
+				if (diffYtoFrom > lastDiffY && wasGoingDown && diffYtoFrom != 0.5) { // 0.5 when use stairs or slab
+					mayCancel = Negativity.alertMod(ReportType.WARNING, p, this,
+							UniversalUtils.parseInPorcent(diffYtoFrom * 200), "going-down",
+							"Was going down, last y " + lastDiffY + ", current: " + diffYtoFrom + ". Down: "
+									+ locDown.getBlock().getType().getId() + ", Down Down: "
+									+ locDownDown.getBlock().getType().getId());
 				}
 			}
-		}
-		np.doubles.set(AIR_JUMP, "diff-y", diffYtoFrom);
-		np.booleans.set(AIR_JUMP, "going-down", diffYtoFrom < 0);
-		if (isSetBack() && mayCancel)
-			LocationUtils.teleportPlayerOnGround(p);
+			np.doubles.set(AIR_JUMP, "diff-y", diffYtoFrom);
+			np.booleans.set(AIR_JUMP, "going-down", diffYtoFrom < 0);
+			if (isSetBack() && mayCancel)
+				LocationUtils.teleportPlayerOnGround(p);
+		}, 5);
 	}
 }

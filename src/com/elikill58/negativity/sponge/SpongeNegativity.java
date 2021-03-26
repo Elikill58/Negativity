96767074d468ecba44ba85c01ccbacc02791baed
Elikill58 <arpetzouille@gmail.com>
2021-03-26 22:53:31 +0100

    Upgrade to 1.9.8-fix

--- src/com/elikill58/negativity/sponge/SpongeNegativity.java
+++ src/com/elikill58/negativity/sponge/SpongeNegativity.java
@@ -118,7 +118,7 @@ import com.google.inject.Inject;
 
 import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
 
-@Plugin(id = "negativity", name = "Negativity", version = "1.9.8", description = "It's an Advanced AntiCheat Detection", authors = { "Elikill58", "RedNesto" }, dependencies = {
+@Plugin(id = "negativity", name = "Negativity", version = "1.9.8-fix", description = "It's an Advanced AntiCheat Detection", authors = { "Elikill58", "RedNesto" }, dependencies = {
 		@Dependency(id = "packetgate") })
 public class SpongeNegativity {
 

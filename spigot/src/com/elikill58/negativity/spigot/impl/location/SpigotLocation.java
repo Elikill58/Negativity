9293c9f8b6fbbba14b811fec4cd8b3428961ba80
RedNesto <RedNestoMC@gmail.com>
2021-06-07 09:09:23 +0200

    Simplify Spigot/SpongeLocation implementation

--- spigot/src/com/elikill58/negativity/spigot/impl/location/SpigotLocation.java
+++ spigot/src/com/elikill58/negativity/spigot/impl/location/SpigotLocation.java
@@ -6,33 +6,26 @@ import com.elikill58.negativity.api.location.World;
 import com.elikill58.negativity.spigot.impl.block.SpigotBlock;
 
 public class SpigotLocation extends Location {
-
-	private final org.bukkit.Location loc;
 	
 	public SpigotLocation(org.bukkit.Location loc) {
 		super(new SpigotWorld(loc.getWorld()), loc.getX(), loc.getY(), loc.getZ());
-		this.loc = loc;
 	}
 	
 	public SpigotLocation(World w, double x, double y, double z) {
 		super(w, x, y, z);
-		this.loc = new org.bukkit.Location((org.bukkit.World) w.getDefault(), x, y, z);
 	}
-
+	
 	@Override
 	public Block getBlock() {
-		return new SpigotBlock(new org.bukkit.Location(loc.getWorld(), getX(), getY(), getZ()).getBlock());
+		return new SpigotBlock(getBukkitWorld().getBlockAt(getBlockX(), getBlockY(), getBlockZ()));
 	}
-
+	
 	@Override
 	public Object getDefault() {
-		org.bukkit.Location loc = this.loc.clone();
-		loc.setX(getX());
-		loc.setY(getY());
-		loc.setZ(getZ());
-		loc.setYaw(getYaw());
-		loc.setPitch(getPitch());
-		return loc;
+		return new org.bukkit.Location(getBukkitWorld(), getX(), getY(), getZ());
 	}
-
+	
+	private org.bukkit.World getBukkitWorld() {
+		return (org.bukkit.World) getWorld().getDefault();
+	}
 }
--- sponge/src/com/elikill58/negativity/sponge/impl/location/SpongeLocation.java
+++ sponge/src/com/elikill58/negativity/sponge/impl/location/SpongeLocation.java
@@ -9,25 +9,25 @@ import com.elikill58.negativity.sponge.impl.block.SpongeBlock;
 
 public class SpongeLocation extends Location {
 
-	private final org.spongepowered.api.world.Location<World> loc;
-	
 	public SpongeLocation(org.spongepowered.api.world.Location<World> location) {
 		super(new SpongeWorld(location.getExtent()), location.getX(), location.getY(), location.getZ());
-		this.loc = location;
 	}
 	
 	public SpongeLocation(com.elikill58.negativity.api.location.World w, double x, double y, double z) {
 		super(w, x, y, z);
-		this.loc = new org.spongepowered.api.world.Location<World>((World) w.getDefault(), x, y, z);
 	}
 
 	@Override
 	public Block getBlock() {
-		return new SpongeBlock(new org.spongepowered.api.world.Location<Extent>(loc.getExtent(), getX(), getY(), getZ()).getBlock().snapshotFor(loc));
+		return new SpongeBlock(getSpongeExtent().createSnapshot(getBlockX(), getBlockY(), getBlockZ()));
 	}
 
 	@Override
 	public Object getDefault() {
-		return new org.spongepowered.api.world.Location<Extent>(loc.getExtent(), getX(), getY(), getZ());
+		return new org.spongepowered.api.world.Location<>(getSpongeExtent(), getX(), getY(), getZ());
+	}
+	
+	private Extent getSpongeExtent() {
+		return (Extent) getWorld().getDefault();
 	}
 }

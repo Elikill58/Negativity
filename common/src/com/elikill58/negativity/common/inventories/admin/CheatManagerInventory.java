196d176ec7d9ae9160aef8181105ed2c8d593415
Elikill58 <arpetzouille@gmail.com>
2021-12-07 16:02:19 +0100

    Show active state in lore

--- common/src/com/elikill58/negativity/common/inventories/admin/CheatManagerInventory.java
+++ common/src/com/elikill58/negativity/common/inventories/admin/CheatManagerInventory.java
@@ -1,5 +1,6 @@
 package com.elikill58.negativity.common.inventories.admin;
 
+import com.elikill58.negativity.api.colors.ChatColor;
 import com.elikill58.negativity.api.entity.Player;
 import com.elikill58.negativity.api.events.inventory.InventoryClickEvent;
 import com.elikill58.negativity.api.inventory.AbstractInventory;
@@ -25,7 +26,8 @@ public class CheatManagerInventory extends AbstractInventory<CheatManagerHolder>
 		int slot = 0;
 		for(Cheat c : Cheat.values())
 			if(c.getMaterial() != null)
-				inv.set(slot++, ItemBuilder.Builder(c.getMaterial()).displayName(c.getName()).build());
+				inv.set(slot++, ItemBuilder.Builder(c.getMaterial()).displayName(c.getName())
+						.lore(ChatColor.GRAY + "State: " + Messages.getMessage(p, "inventory.manager." + (c.isActive() ? "enabled" : "disabled"))).build());
 
 		inv.set(inv.getSize() - 2, ItemBuilder.Builder(Materials.ARROW).displayName(Messages.getMessage(p, "inventory.back")).build());
 		inv.set(inv.getSize() - 1, ItemBuilder.Builder(Materials.BARRIER).displayName(Messages.getMessage(p, "inventory.close")).build());

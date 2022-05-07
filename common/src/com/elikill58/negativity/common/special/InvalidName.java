9baaa6e23485af33aac0699ccfcc412c74bcf098
Elikill58 <arpetzouille@gmail.com>
2022-05-07 14:27:29 +0200

    Prevent bungee wrong floodgate compatibility

--- common/src/com/elikill58/negativity/common/special/InvalidName.java
+++ common/src/com/elikill58/negativity/common/special/InvalidName.java
@@ -9,6 +9,7 @@ import com.elikill58.negativity.api.events.player.LoginEvent.Result;
 import com.elikill58.negativity.api.item.Materials;
 import com.elikill58.negativity.universal.Adapter;
 import com.elikill58.negativity.universal.Messages;
+import com.elikill58.negativity.universal.ProxyCompanionManager;
 import com.elikill58.negativity.universal.account.NegativityAccount;
 import com.elikill58.negativity.universal.ban.Ban;
 import com.elikill58.negativity.universal.ban.BanManager;
@@ -30,26 +31,34 @@ public class InvalidName extends Special implements Listeners {
 			return;
 		UUID playerId = e.getUUID();
 		NegativityAccount account = NegativityAccount.get(playerId);
-		if (!UniversalUtils.isValidName(e.getName()) && !BedrockPlayerManager.isBedrockPlayer(playerId)) {
-			// check for ban / kick only if the player is not already banned
-			if (getConfig().getBoolean("ban")) {
-				if (!BanManager.banActive) {
-					Adapter ada = Adapter.getAdapter();
-					ada.getLogger().warn("Cannot ban player " + e.getName() + " for " + getName() + " because ban is NOT config.");
-					ada.getLogger().warn("Please, enable ban in config and restart your server");
-					if (getConfig().getBoolean("kick")) {
-						e.setKickMessage(Messages.getMessage(account, "kick.kicked", "%name%", "Negativity", "%reason%", getName()));
-						e.setLoginResult(Result.KICK_OTHER);
-					}
-				} else {
-					BanManager.executeBan(Ban.active(playerId, getName(), "Negativity", BanType.PLUGIN,
-							getConfig().getInt("ban.time"), getName(), e.getAddress().getHostAddress()));
-					e.setLoginResult(Result.KICK_BANNED);
+		if (UniversalUtils.isValidName(e.getName())) // valid name, ignoring
+			return;
+		if (BedrockPlayerManager.isBedrockPlayer(playerId)) // bedrock player, we are sure about this
+			return;
+		if (ProxyCompanionManager.isIntegrationEnabled() && UniversalUtils.isValidName(e.getName().substring(1)))
+			return; // seems to be geyser player with invalid char at begin
+
+		// check for ban / kick only if the player is not already banned
+		if (getConfig().getBoolean("ban")) {
+			if (!BanManager.banActive) {
+				Adapter ada = Adapter.getAdapter();
+				ada.getLogger()
+						.warn("Cannot ban player " + e.getName() + " for " + getName() + " because ban is NOT config.");
+				ada.getLogger().warn("Please, enable ban in config and restart your server");
+				if (getConfig().getBoolean("kick")) {
+					e.setKickMessage(
+							Messages.getMessage(account, "kick.kicked", "%name%", "Negativity", "%reason%", getName()));
+					e.setLoginResult(Result.KICK_OTHER);
 				}
-			} else if (getConfig().getBoolean("kick")) {
-				e.setKickMessage(Messages.getMessage(account, "kick.kicked", "%name%", "Negativity", "%reason%", getName()));
-				e.setLoginResult(Result.KICK_OTHER);
+			} else {
+				BanManager.executeBan(Ban.active(playerId, getName(), "Negativity", BanType.PLUGIN,
+						getConfig().getInt("ban.time"), getName(), e.getAddress().getHostAddress()));
+				e.setLoginResult(Result.KICK_BANNED);
 			}
+		} else if (getConfig().getBoolean("kick")) {
+			e.setKickMessage(
+					Messages.getMessage(account, "kick.kicked", "%name%", "Negativity", "%reason%", getName()));
+			e.setLoginResult(Result.KICK_OTHER);
 		}
 	}
 }

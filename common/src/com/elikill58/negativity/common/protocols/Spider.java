56c2d510d8dcd1c0cd9f57e338e6759507316100
Elikill58 <arpetzouille@gmail.com>
2022-05-09 20:04:10 +0200

    Quickly change for spider

--- common/src/com/elikill58/negativity/common/protocols/Spider.java
+++ common/src/com/elikill58/negativity/common/protocols/Spider.java
@@ -25,45 +25,48 @@ public class Spider extends Cheat {
 		super(SPIDER, CheatCategory.MOVEMENT, Materials.SPIDER_EYE);
 	}
 
-	@Check(name = "nothing-around", description = "Walking with nothing around", conditions = { CheckConditions.SURVIVAL, CheckConditions.NO_USE_ELEVATOR, CheckConditions.NO_ELYTRA, CheckConditions.NO_FLY, CheckConditions.NO_FALL_DISTANCE, CheckConditions.NO_SPRINT, CheckConditions.NO_USE_SLIME })
+	@Check(name = "nothing-around", description = "Walking with nothing around", conditions = {
+			CheckConditions.SURVIVAL, CheckConditions.NO_USE_ELEVATOR, CheckConditions.NO_ELYTRA,
+			CheckConditions.NO_FLY, CheckConditions.NO_FALL_DISTANCE, CheckConditions.NO_SPRINT, CheckConditions.NO_USE_TRIDENT,
+			CheckConditions.NO_USE_SLIME })
 	public void onPlayerMove(PlayerMoveEvent e, NegativityPlayer np) {
 		Player p = e.getPlayer();
 		Location loc = p.getLocation();
-		if(Version.getVersion().isNewerOrEquals(Version.V1_9) && p.hasPotionEffect(PotionEffectType.LEVITATION))
+		if (Version.getVersion().isNewerOrEquals(Version.V1_9) && p.hasPotionEffect(PotionEffectType.LEVITATION))
 			return;
-		if(p.hasPotionEffect(PotionEffectType.JUMP) || np.booleans.get(CheatKeys.ALL, "jump-boost-use", false))
+		if (p.hasPotionEffect(PotionEffectType.JUMP) || np.booleans.get(CheatKeys.ALL, "jump-boost-use", false))
 			return;
 		Location from = e.getFrom(), to = e.getTo();
 		double y = to.getY() - from.getY();
 		if (!LocationUtils.hasOtherThan(loc, Materials.AIR) || (from.getX() == to.getX() && from.getZ() == to.getZ()))
-				return;
+			return;
 		Material underPlayer = loc.clone().sub(0, 1, 0).getBlock().getType(),
 				underUnder = loc.clone().sub(0, 2, 0).getBlock().getType();
 		if (!underPlayer.equals(Materials.AIR) || !underUnder.equals(Materials.AIR)
 				|| !loc.getBlock().getType().equals(Materials.AIR))
 			return;
-		if (p.getItemInHand() != null && p.getItemInHand().getType().getId().contains("TRIDENT"))
-			return;
-		if(hasBypassBlockAround(loc))
+		if (hasBypassBlockAround(loc))
 			return;
 		boolean isAris = ((float) y) == p.getWalkSpeed();
 		if (((y > 0.499 && y < 0.7) || isAris) && p.getVelocity().length() < 1.5) {
 			int relia = UniversalUtils.parseInPorcent(y * 160 + (isAris ? 39 : 0));
-			boolean mayCancel = Negativity.alertMod((np.getWarn(this) > 6 ? ReportType.WARNING : ReportType.VIOLATION), p, this, relia,
-					"nothing-around", "Nothing around him. To > From: " + y + " isAris: " + isAris + ", has not stab slairs")
+			boolean mayCancel = Negativity.alertMod((np.getWarn(this) > 6 ? ReportType.WARNING : ReportType.VIOLATION),
+					p, this, relia, "nothing-around",
+					"Nothing around him. To > From: " + y + " isAris: " + isAris + ", has not stab slairs")
 					&& isSetBack();
-			if(mayCancel)
+			if (mayCancel)
 				LocationUtils.teleportPlayerOnGround(p);
 		}
 	}
 
-	@Check(name = "same-y", description = "Player move with same Y", conditions = { CheckConditions.SURVIVAL, CheckConditions.NO_USE_ELEVATOR,
-			CheckConditions.NO_ELYTRA, CheckConditions.NO_FLY, CheckConditions.NO_FALL_DISTANCE })
+	@Check(name = "same-y", description = "Player move with same Y", conditions = { CheckConditions.SURVIVAL,
+			CheckConditions.NO_USE_ELEVATOR, CheckConditions.NO_ELYTRA, CheckConditions.NO_FLY,
+			CheckConditions.NO_FALL_DISTANCE })
 	public void onPlayerMoveSameY(PlayerMoveEvent e, NegativityPlayer np) {
 		Player p = e.getPlayer();
-		if(Version.getVersion().isNewerOrEquals(Version.V1_9) && p.hasPotionEffect(PotionEffectType.LEVITATION))
+		if (Version.getVersion().isNewerOrEquals(Version.V1_9) && p.hasPotionEffect(PotionEffectType.LEVITATION))
 			return;
-		if(p.hasPotionEffect(PotionEffectType.JUMP) || np.booleans.get(CheatKeys.ALL, "jump-boost-use", false))
+		if (p.hasPotionEffect(PotionEffectType.JUMP) || np.booleans.get(CheatKeys.ALL, "jump-boost-use", false))
 			return;
 		int amount = 0;
 		Location from = e.getFrom(), to = e.getTo();
@@ -90,35 +93,41 @@ public class Spider extends Cheat {
 			}
 		}
 		if (amount > 1) {
-			boolean mayCancel = (Negativity.alertMod((np.getWarn(this) > 6 ? ReportType.WARNING : ReportType.VIOLATION), p, this, UniversalUtils.parseInPorcent(80 + amount * 3),
-					"nothing-around", "Y: " + y + ", fall: " + p.getFallDistance() + ", aount: " + amount)
-					&& isSetBack());
-			if(mayCancel)
+			boolean mayCancel = (Negativity.alertMod((np.getWarn(this) > 6 ? ReportType.WARNING : ReportType.VIOLATION),
+					p, this, UniversalUtils.parseInPorcent(80 + amount * 3), "nothing-around",
+					"Y: " + y + ", fall: " + p.getFallDistance() + ", aount: " + amount) && isSetBack());
+			if (mayCancel)
 				LocationUtils.teleportPlayerOnGround(p);
 		}
 		np.lastY.add(y);
 	}
-	
-	@Check(name = "distance", description = "Distance when going up", conditions = { CheckConditions.SURVIVAL, CheckConditions.NO_BLOCK_MID_AROUND_BELOW,
-			CheckConditions.NO_INSIDE_VEHICLE, CheckConditions.NO_USE_ELEVATOR, CheckConditions.NO_LIQUID_AROUND,
-			CheckConditions.NO_STAIRS_AROUND_EXTENDED, CheckConditions.NO_USE_TRIDENT, CheckConditions.NO_BLOCK_MID_AROUND, CheckConditions.NO_FLY })
+
+	@Check(name = "distance", description = "Distance when going up", conditions = { CheckConditions.SURVIVAL,
+			CheckConditions.NO_BLOCK_MID_AROUND_BELOW, CheckConditions.NO_INSIDE_VEHICLE,
+			CheckConditions.NO_USE_ELEVATOR, CheckConditions.NO_LIQUID_AROUND,
+			CheckConditions.NO_STAIRS_AROUND_EXTENDED, CheckConditions.NO_USE_TRIDENT,
+			CheckConditions.NO_BLOCK_MID_AROUND, CheckConditions.NO_FLY })
 	public void onPlayerContinueMove(PlayerMoveEvent e, NegativityPlayer np) {
 		Player p = e.getPlayer();
-		if(Version.getVersion().isNewerOrEquals(Version.V1_9) && p.hasPotionEffect(PotionEffectType.LEVITATION))
+		if (Version.getVersion().isNewerOrEquals(Version.V1_9) && p.hasPotionEffect(PotionEffectType.LEVITATION))
 			return;
 		Location loc = p.getLocation().clone();
 		String blockName = p.getLocation().getBlock().getType().getId();
-		if(blockName.contains("LADDER") || blockName.contains("VINE"))
+		if (blockName.contains("LADDER") || blockName.contains("VINE"))
 			return;
-		
+
 		double y = e.getTo().getY() - e.getFrom().getY();
 		if (np.lastSpiderLoc != null && np.lastSpiderLoc.getWorld().equals(loc.getWorld()) && y > 0) {
-			double tempDis = loc.getY() - np.lastSpiderLoc.getY(), lastSpiderDistance = np.doubles.get(SPIDER, "last-distance", 0.0);
+			double tempDis = loc.getY() - np.lastSpiderLoc.getY(),
+					lastSpiderDistance = np.doubles.get(SPIDER, "last-distance", 0.0);
 			if (lastSpiderDistance == tempDis && tempDis != 0) {
 				np.spiderSameDist++;
-				if(np.spiderSameDist > 2) {
-					if (Negativity.alertMod(ReportType.WARNING, p, this, UniversalUtils.parseInPorcent(tempDis * 400 + np.spiderSameDist), "distance",
-							"Nothing strange around him. To > From: " + y + ", distance: " + lastSpiderDistance + ". Walk with same y " + np.spiderSameDist + " times") && isSetBack()) {
+				if (np.spiderSameDist > 2) {
+					if (Negativity.alertMod(ReportType.WARNING, p, this,
+							UniversalUtils.parseInPorcent(tempDis * 400 + np.spiderSameDist), "distance",
+							"Nothing strange around him. To > From: " + y + ", distance: " + lastSpiderDistance
+									+ ". Walk with same y " + np.spiderSameDist + " times")
+							&& isSetBack()) {
 						LocationUtils.teleportPlayerOnGround(p);
 					}
 				}
@@ -129,30 +138,29 @@ public class Spider extends Cheat {
 			np.spiderSameDist = 0;
 		np.lastSpiderLoc = loc;
 	}
-		
+
 	private boolean hasBypassBlockAround(Location loc) {
-		if(has(loc, "SLAB", "STAIRS", "VINE", "LADDER", "WATER", "SCAFFOLD", "CAKE", "SNOW"))
+		if (has(loc, "SLAB", "STAIRS", "VINE", "LADDER", "WATER", "SCAFFOLD", "CAKE", "SNOW"))
 			return true;
 		loc = loc.clone().sub(0, 1, 0);
 		return has(loc, "SLAB", "STAIRS", "VINE", "LADDER", "WATER", "SCAFFOLD", "CAKE", "SNOW");
 	}
 
 	public boolean has(Location loc, String... m) {
-		String b = loc.getBlock().getType().getId(),
-				b1 = loc.clone().add(0, 0, 1).getBlock().getType().getId(),
+		String b = loc.getBlock().getType().getId(), b1 = loc.clone().add(0, 0, 1).getBlock().getType().getId(),
 				b2 = loc.clone().add(1, 0, -1).getBlock().getType().getId(),
 				b3 = loc.clone().add(-1, 0, -1).getBlock().getType().getId(),
 				b4 = loc.clone().add(-1, 0, 1).getBlock().getType().getId();
-		for(String temp : m) {
-			if(b.contains(temp))
+		for (String temp : m) {
+			if (b.contains(temp))
 				return true;
-			if(b1.contains(temp))
+			if (b1.contains(temp))
 				return true;
-			if(b2.contains(temp))
+			if (b2.contains(temp))
 				return true;
-			if(b3.contains(temp))
+			if (b3.contains(temp))
 				return true;
-			if(b4.contains(temp))
+			if (b4.contains(temp))
 				return true;
 		}
 		return false;

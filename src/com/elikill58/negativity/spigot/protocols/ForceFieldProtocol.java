81272d3f04245d66a9be4cd0c8ccc07d794e9330
Elikill58 <arpetzouille@gmail.com>
2022-05-23 22:15:35 +0200

    Run distance calc async

--- src/com/elikill58/negativity/spigot/protocols/ForceFieldProtocol.java
+++ src/com/elikill58/negativity/spigot/protocols/ForceFieldProtocol.java
@@ -65,20 +65,26 @@ public class ForceFieldProtocol extends Cheat implements Listener {
 			Entity cible = e.getEntity();
 			Object pBB = PacketUtils.getBoundingBox(p);
 			Object cibleBB = PacketUtils.getBoundingBox(cible);
-			double oldDistance = distance(pBB, cibleBB);
-			double dis = new Rect(cibleBB).distance(p);
-			Adapter.getAdapter().debug("Distance: " + dis + ", old: " + oldDistance);
-			recordData(p.getUniqueId(), HIT_DISTANCE, dis);
-			Material blockType = p.getLocation().getBlock().getType();
-			double maxReach = Adapter.getAdapter().getConfig().getDouble("cheats.forcefield.reach");
-			if (dis > maxReach && !blockType.name().contains("WATER") && !blockType.name().contains("LAVA")) {
-				String entityName = Version.getVersion().equals(Version.V1_7) ? cible.getType().name().toLowerCase(Locale.ROOT) : cible.getName();
-				boolean mayCancel = SpigotNegativity.alertMod(ReportType.WARNING, p, this, parseInPorcent(dis * (oldDistance > maxReach ? 3 : 2) * 10),
-						"Big distance with: " + cible.getType().name().toLowerCase(Locale.ROOT) + ". Exact distance: " + dis + ", without thorns"
-						+ ". Ping: " + np.ping, hoverMsg("distance", "%name%", entityName, "%distance%", nf.format(dis)));
-				if (isSetBack() && mayCancel)
-					e.setCancelled(true);
-			}
+			Adapter.getAdapter().runAsync(() -> {
+				try {
+					double oldDistance = distance(pBB, cibleBB);
+					double dis = new Rect(cibleBB).distance(p);
+					Adapter.getAdapter().debug("Distance: " + dis + ", old: " + oldDistance);
+					recordData(p.getUniqueId(), HIT_DISTANCE, dis);
+					Material blockType = p.getLocation().getBlock().getType();
+					double maxReach = Adapter.getAdapter().getConfig().getDouble("cheats.forcefield.reach");
+					if (dis > maxReach && !blockType.name().contains("WATER") && !blockType.name().contains("LAVA")) {
+						String entityName = Version.getVersion().equals(Version.V1_7) ? cible.getType().name().toLowerCase(Locale.ROOT) : cible.getName();
+						boolean mayCancel = SpigotNegativity.alertMod(ReportType.WARNING, p, this, parseInPorcent(dis * (oldDistance > maxReach ? 3 : 2) * 10),
+								"Big distance with: " + cible.getType().name().toLowerCase(Locale.ROOT) + ". Exact distance: " + dis + ", without thorns"
+								+ ". Ping: " + np.ping, hoverMsg("distance", "%name%", entityName, "%distance%", nf.format(dis)));
+						if (isSetBack() && mayCancel)
+							e.setCancelled(true);
+					}
+				} catch (Exception exc) {
+					exc.printStackTrace();
+				}
+			});
 		}
 	}
 	

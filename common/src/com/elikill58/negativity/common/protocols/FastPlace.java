38d33e33cb418e87adac9adc4bc2031504bc96bc
Elikill58 <arpetzouille@gmail.com>
2022-06-23 11:48:27 +0200

    Upgrade FastPlace check

--- common/src/com/elikill58/negativity/common/protocols/FastPlace.java
+++ common/src/com/elikill58/negativity/common/protocols/FastPlace.java
@@ -1,56 +1,56 @@
 package com.elikill58.negativity.common.protocols;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.elikill58.negativity.api.NegativityPlayer;
 import com.elikill58.negativity.api.entity.Player;
-import com.elikill58.negativity.api.events.block.BlockPlaceEvent;
+import com.elikill58.negativity.api.events.packets.PacketReceiveEvent;
 import com.elikill58.negativity.api.item.Materials;
+import com.elikill58.negativity.api.packets.PacketType;
 import com.elikill58.negativity.api.protocols.Check;
-import com.elikill58.negativity.api.protocols.CheckConditions;
-import com.elikill58.negativity.api.utils.Utils;
 import com.elikill58.negativity.universal.Negativity;
 import com.elikill58.negativity.universal.detections.Cheat;
 import com.elikill58.negativity.universal.detections.keys.CheatKeys;
 import com.elikill58.negativity.universal.report.ReportType;
 import com.elikill58.negativity.universal.utils.UniversalUtils;
-import com.elikill58.negativity.universal.verif.VerifData;
-import com.elikill58.negativity.universal.verif.VerifData.DataType;
-import com.elikill58.negativity.universal.verif.data.DataCounter;
-import com.elikill58.negativity.universal.verif.data.LongDataCounter;
 
 public class FastPlace extends Cheat {
 	
-	public static final DataType<Long> TIME_PLACE = new DataType<Long>("time_player", "Time between places", () -> new LongDataCounter());
-
 	public FastPlace() {
-		super(CheatKeys.FAST_PLACE, CheatCategory.WORLD, Materials.DIRT, CheatDescription.VERIF, CheatDescription.BLOCKS);
+		super(CheatKeys.FAST_PLACE, CheatCategory.WORLD, Materials.DIRT, CheatDescription.BLOCKS);
 	}
 
-	@Check(name = "time", description = "Time between 2 place", conditions = { CheckConditions.SURVIVAL })
-	public void onBlockPlace(BlockPlaceEvent e, NegativityPlayer np) {
+	@Check(name = "time", description = "Time between 2 place")
+	public void onBlockPlace(PacketReceiveEvent e, NegativityPlayer np) {
 		Player p = e.getPlayer();
 		
-		int ping = p.getPing();
-		if(ping < 50)
-			ping = 50;
-		long last = System.currentTimeMillis() - np.lastBlockPlace, lastPing = last + (ping / 50) + 1;
-		if(last < 1000) // last block is too old
-			recordData(p.getUniqueId(), TIME_PLACE, last);
-		np.lastBlockPlace = System.currentTimeMillis();
-		if ((lastPing + 5) < getConfig().getInt("time_2_place", 50)) { // 5 is to prevent some processing time issue
-			boolean mayCancel = Negativity.alertMod(ReportType.WARNING, p, this, UniversalUtils.parseInPorcent(lastPing <= 5 ? 100 - lastPing : 100 - lastPing * 2),
-					"time", "Block placed too quickly. Last time: " + last + ", Last with ping: "
-					+ lastPing + ".", hoverMsg("main", "%time%", last));
-			if(isSetBack() && mayCancel)
-				e.setCancelled(true);
+		if(e.getPacket().getPacketType().isFlyingPacket()) {
+			np.ints.set(getKey(), "times-flying", np.ints.get(getKey(), "times-flying", 0) + 1);
+		} else if(e.getPacket().getPacketType().equals(PacketType.Client.BLOCK_PLACE)) {
+			Integer flying = np.ints.remove(getKey(), "times-flying");
+			if(flying == null)
+				return;
+			if(flying < 20) {
+				List<Integer> list = np.listIntegers.get(getKey(), "times", new ArrayList<>());
+				list.add(flying);
+				if(list.size() >= 2) {
+					double total = 0;
+					for(Integer temp : list)
+						total += temp;
+					double average = (total / list.size());
+					
+					if (average < 3) {
+						boolean mayCancel = Negativity.alertMod(ReportType.WARNING, p, this, UniversalUtils.parseInPorcent(100 - average * 5),
+								"time", "Quickly: " + String.format("%.3f", average) + ", " + list);
+						if(isSetBack() && mayCancel)
+							e.setCancelled(true);
+					}
+				}
+			} else {
+				if(flying > 200) // if outdated
+					np.listIntegers.remove(getKey(), "times");
+			}
 		}
-	}
-	
-	@Override
-	public String makeVerificationSummary(VerifData data, NegativityPlayer np) {
-		DataCounter<Long> counter = data.getData(TIME_PLACE);
-		long av = counter.getAverage(), low = counter.getMin();
-		String colorAverage = (av < 100 ? (av < 20 ? "&c" : "&6") : "&a");
-		String colorLow = (low < 100 ? (low < 20 ? "&c" : "&6") : "&a");
-		return Utils.coloredMessage("&6Time between place: &7Average: " + colorAverage + av + "&7, Lower: " + colorLow + low + " &7(For " + counter.getSize() + " blocks)");
 	}
 }

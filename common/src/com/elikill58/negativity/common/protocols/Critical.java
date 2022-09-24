36a94f8fe51b09b7538ba0cec849abd17be209c7
Elikill58 <arpetzouille@gmail.com>
2022-09-24 17:58:36 +0200

    Upgrade new critical check

--- common/src/com/elikill58/negativity/common/protocols/Critical.java
+++ common/src/com/elikill58/negativity/common/protocols/Critical.java
@@ -5,28 +5,31 @@ import java.util.Arrays;
 import com.elikill58.negativity.api.NegativityPlayer;
 import com.elikill58.negativity.api.entity.Player;
 import com.elikill58.negativity.api.events.EventListener;
+import com.elikill58.negativity.api.events.Listeners;
 import com.elikill58.negativity.api.events.packets.PacketReceiveEvent;
+import com.elikill58.negativity.api.events.player.PlayerDamageEntityEvent;
 import com.elikill58.negativity.api.events.player.PlayerDamagedByEntityEvent;
 import com.elikill58.negativity.api.item.Materials;
-import com.elikill58.negativity.api.packets.PacketType.Client;
-import com.elikill58.negativity.api.packets.packet.playin.NPacketPlayInPositionLook;
+import com.elikill58.negativity.api.packets.packet.playin.NPacketPlayInFlying;
 import com.elikill58.negativity.api.protocols.Check;
 import com.elikill58.negativity.api.protocols.CheckConditions;
 import com.elikill58.negativity.common.protocols.data.CriticalData;
+import com.elikill58.negativity.common.protocols.data.CriticalData.TimedFlyingPacket;
 import com.elikill58.negativity.universal.Negativity;
 import com.elikill58.negativity.universal.Version;
 import com.elikill58.negativity.universal.detections.Cheat;
 import com.elikill58.negativity.universal.detections.keys.CheatKeys;
 import com.elikill58.negativity.universal.report.ReportType;
+import com.elikill58.negativity.universal.utils.UniversalUtils;
 
-public class Critical extends Cheat {
+public class Critical extends Cheat implements Listeners {
 	
 	public Critical() {
 		super(CheatKeys.CRITICAL, CheatCategory.COMBAT, Materials.FIREBALL, CriticalData::new);
 	}
 
 	@Check(name = "ground", description = "Check damage according to Y", conditions = { CheckConditions.NO_INSIDE_VEHICLE, CheckConditions.SURVIVAL, CheckConditions.NO_FLY, CheckConditions.NO_GROUND })
-	public void onDamage(PlayerDamagedByEntityEvent e, NegativityPlayer np, CriticalData data) {
+	public void onDamaged(PlayerDamagedByEntityEvent e, NegativityPlayer np, CriticalData data) {
 		if (e.isCancelled())
 			return;
 		Player p = e.getPlayer();
@@ -41,28 +44,39 @@ public class Critical extends Cheat {
 	}
 
 	@Check(name = "y-pos", description = "Check for Y move before attack")
-	public void onDamageA(PlayerDamagedByEntityEvent e, NegativityPlayer np, CriticalData data) {
+	public void onPlayerDamage(PlayerDamageEntityEvent e, NegativityPlayer np, CriticalData data) {
 		if (e.isCancelled())
 			return;
-		NPacketPlayInPositionLook l1 = data.positions.get(3);
-		NPacketPlayInPositionLook l2 = data.positions.get(2);
-		NPacketPlayInPositionLook l3 = data.positions.get(1);
-		NPacketPlayInPositionLook base = data.positions.get(0);
-		for(NPacketPlayInPositionLook checkingLocs : Arrays.asList(l1, l2, l3)) // check for no X/Z move
-			if(checkingLocs.getX() != base.getX() || checkingLocs.getZ() != base.getZ())
+		TimedFlyingPacket l3 = data.positions.get(3);
+		TimedFlyingPacket l2 = data.positions.get(2);
+		TimedFlyingPacket l1 = data.positions.get(1);
+		TimedFlyingPacket base = data.positions.get(0);
+		for(TimedFlyingPacket checkingLocs : Arrays.asList(l2, l3)) // check for no X/Z move
+			if(checkingLocs.getX() != l1.getX() || checkingLocs.getZ() != l1.getZ() || checkingLocs.isGround())
 				return;
 		double baseY = base.getY();
-		if(baseY + 0.11 == l3.getY() && baseY + 0.1100013579 == l3.getY() && baseY + 0.0000013579 == l3.getY()) {
-			boolean mayCancel = Negativity.alertMod(ReportType.WARNING, np.getPlayer(), this, 100, "y-pos", "Positions: " + data.positions);
-			if(mayCancel && isSetBack())
-				e.setCancelled(true);
+		if(baseY + 0.11 == l1.getY() && baseY + 0.1100013579 == l2.getY() && baseY + 0.0000013579 == l3.getY()) {
+			Player p = e.getPlayer();
+			long timeFor3Packets = l3.getTime() - l1.getTime();
+			long tmpPing = p.getPing();
+			if(tmpPing < 50) // 1 tick
+				tmpPing = 50;
+			long diffPacket = tmpPing - timeFor3Packets;
+			if(diffPacket > 0) {
+				boolean mayCancel = Negativity.alertMod(ReportType.WARNING, p, this, UniversalUtils.parseInPorcent(101 - timeFor3Packets), "y-pos", "Positions: " + data.positions + ", time3Packet: " + timeFor3Packets + ", diffPinged: " + diffPacket, null, diffPacket);
+				if(mayCancel && isSetBack())
+					e.setCancelled(true);
+			}
 		}
 	}
 	
 	@EventListener
 	public void onPacket(PacketReceiveEvent e) {
-		if(e.getPacket().getPacketType().equals(Client.POSITION_LOOK)) {
-			NegativityPlayer.getNegativityPlayer(e.getPlayer()).<CriticalData>getCheckData(this).add((NPacketPlayInPositionLook) e.getPacket().getPacket());
+		if(e.getPacket().getPacketType().isFlyingPacket()) {
+			NPacketPlayInFlying flying = (NPacketPlayInFlying) e.getPacket().getPacket();
+			if(flying.hasLocation()) {
+				NegativityPlayer.getNegativityPlayer(e.getPlayer()).<CriticalData>getCheckData(this).add(flying);
+			}
 		}
 	}
 }

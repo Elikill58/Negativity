8239e91813fb6a036ddfed95e0e84c826f75d640
Elikill58 <arpetzouille@gmail.com>
2022-11-14 22:35:18 +0100

    Make common handler for netty decode/encode

--- common/src/com/elikill58/negativity/api/packets/nms/channels/netty/NettyEncoderHandler.java
+++ common/src/com/elikill58/negativity/api/packets/nms/channels/netty/NettyEncoderHandler.java
@@ -1,17 +1,12 @@
 package com.elikill58.negativity.api.packets.nms.channels.netty;
 
-import java.nio.channels.ClosedChannelException;
 import java.util.List;
-import java.util.Locale;
 
 import com.elikill58.negativity.api.entity.Player;
 import com.elikill58.negativity.api.events.EventManager;
 import com.elikill58.negativity.api.events.packets.PacketSendEvent;
 import com.elikill58.negativity.api.packets.PacketDirection;
-import com.elikill58.negativity.api.packets.nms.NamedVersion;
-import com.elikill58.negativity.api.packets.nms.PacketSerializer;
 import com.elikill58.negativity.api.packets.packet.NPacket;
-import com.elikill58.negativity.universal.Adapter;
 import com.elikill58.negativity.universal.Version;
 import com.elikill58.negativity.universal.multiVersion.PlayerVersionManager;
 
@@ -23,39 +18,23 @@ public class NettyEncoderHandler extends MessageToMessageEncoder<ByteBuf> {
 
 	private final Player p;
 	private final PacketDirection direction;
-	private final Version v;
-	private final NamedVersion version;
+	private final Version version;
 	
 	public NettyEncoderHandler(Player p, PacketDirection direction) {
 		this.p = p;
 		this.direction = direction;
-		this.version = (v = PlayerVersionManager.getPlayerVersion(p)).getOrCreateNamedVersion();
+		this.version = PlayerVersionManager.getPlayerVersion(p);
 	}
 	
 	@Override
 	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-		if(cause.getMessage().toLowerCase(Locale.ENGLISH).contains("connection reset by ")
-				|| cause.getLocalizedMessage().toLowerCase(Locale.ENGLISH).contains("connection reset by "))
-			return;
-		if(cause instanceof ClosedChannelException)
-			return;
-		Adapter.getAdapter().getLogger().error("Exception caught when sending packet");
-		cause.printStackTrace();
+		NettyHandlerCommon.manageError(ctx, cause);
 	}
 
 	@Override
 	protected void encode(ChannelHandlerContext ctx, ByteBuf msg, List<Object> out) throws Exception {
-		ByteBuf buf = msg.copy();
-		int packetId = new PacketSerializer(buf).readVarInt();
-		NPacket packet = version.getPacket(direction, packetId);
+		NPacket packet = NettyHandlerCommon.readPacketFromByteBuf(p, version, direction, ctx, msg.copy(), "encode");
 		if(packet == null) {
-			out.add(msg.retain());
-			return;
-		}
-		try {
-			packet.read(new PacketSerializer(buf), v);
-		} catch (IndexOutOfBoundsException e) {
-			Adapter.getAdapter().getLogger().warn("Failed to read packet with ID " + packetId + " for player " + p.getName() + " (dir: " + direction.name() + " - Encode)");
 			out.add(msg.retain());
 			return;
 		}
---
+++ common/src/com/elikill58/negativity/api/packets/nms/channels/netty/NettyHandlerCommon.java
@@ -0,0 +1,59 @@
+package com.elikill58.negativity.api.packets.nms.channels.netty;
+
+import java.nio.channels.ClosedChannelException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import com.elikill58.negativity.api.entity.Player;
+import com.elikill58.negativity.api.packets.PacketDirection;
+import com.elikill58.negativity.api.packets.nms.PacketSerializer;
+import com.elikill58.negativity.api.packets.packet.NPacket;
+import com.elikill58.negativity.api.packets.packet.NPacketUnset;
+import com.elikill58.negativity.universal.Adapter;
+import com.elikill58.negativity.universal.Version;
+import com.elikill58.negativity.universal.logger.LoggerAdapter;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+
+public class NettyHandlerCommon {
+
+	private static final List<String> sentMessages = new ArrayList<>(); 
+	
+	public static void manageError(ChannelHandlerContext ctx, Throwable cause) {
+		if (cause.getMessage().toLowerCase(Locale.ENGLISH).contains("connection reset by ")
+				|| cause.getLocalizedMessage().toLowerCase(Locale.ENGLISH).contains("connection reset by "))
+			return;
+		if (cause instanceof ClosedChannelException)
+			return;
+		Adapter.getAdapter().getLogger().error("Exception caught when sending packet");
+		cause.printStackTrace();
+	}
+
+	public static NPacket readPacketFromByteBuf(Player p, Version version, PacketDirection direction,
+			ChannelHandlerContext ctx, ByteBuf msg, String comment) throws Exception {
+		ByteBuf buf = msg.copy();
+		int packetId = new PacketSerializer(buf).readVarInt();
+		NPacket packet = version.getOrCreateNamedVersion().getPacket(direction, packetId);
+		if (packet == null) {
+			return null;
+		}
+		try {
+			packet.read(new PacketSerializer(buf), version);
+		} catch (IndexOutOfBoundsException e) {
+			LoggerAdapter ada = Adapter.getAdapter().getLogger();
+			ada.warn("Failed to read packet with ID " + packetId + " ("
+					+ (packet instanceof NPacketUnset ? ((NPacketUnset) packet).getPacketTypeCible()
+							: packet.getPacketType())
+					+ ") to " + p.getName() + " (" + direction.name() + " - " + comment + " - " + version.getName() + ")");
+			String key = packetId + "_" + packet.getPacketType() + "_" + e.getMessage();
+			if(sentMessages.contains(key))
+				return packet;
+			sentMessages.add(key);
+			ada.warn(e.getMessage());
+			e.printStackTrace();
+		}
+		return packet;
+	}
+}

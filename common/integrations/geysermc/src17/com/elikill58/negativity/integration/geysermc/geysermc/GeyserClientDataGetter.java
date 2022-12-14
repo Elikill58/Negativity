135ec4a7fda3465eae158314157df618cead5f45
Elikill58 <arpetzouille@gmail.com>
2022-12-14 10:27:35 +0100

    Fix geysermc not included

---
+++ common/integrations/geysermc/src17/com/elikill58/negativity/integration/geysermc/geysermc/GeyserClientDataGetter.java
@@ -0,0 +1,28 @@
+package com.elikill58.negativity.integration.geysermc.geysermc;
+
+import java.util.UUID;
+
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.geysermc.floodgate.api.FloodgateApi;
+import org.geysermc.geyser.GeyserImpl;
+import org.geysermc.geyser.session.GeyserSession;
+
+import com.elikill58.negativity.api.packets.BedrockClientData;
+import com.elikill58.negativity.api.packets.BedrockClientData.BedrockOs;
+import com.elikill58.negativity.universal.Adapter;
+import com.elikill58.negativity.universal.bedrock.data.BedrockClientDataGetter;
+
+public class GeyserClientDataGetter implements BedrockClientDataGetter {
+	
+	@Override
+	public @Nullable BedrockClientData getClientData(UUID uuid) {
+		GeyserSession session = GeyserImpl.getInstance().connectionByUuid(uuid);
+		if(session == null || session.getClientData() == null)
+			return null;
+		org.geysermc.geyser.session.auth.BedrockClientData data = session.getClientData();
+		Adapter ada = Adapter.getAdapter();
+		BedrockOs os = BedrockOs.valueOf(ada.hasPlugin("floodgate") ? FloodgateApi.getInstance().getPlayer(uuid).getDeviceOs().name() : data.getDeviceOs().name());
+		
+		return new BedrockClientData(data.getDeviceId(), data.getDeviceModel(), os);
+	}
+}
---
+++ common/integrations/geysermc/src8/com/elikill58/negativity/integration/geysermc/geysermc/GeyserClientDataProvider.java
@@ -0,0 +1,25 @@
+package com.elikill58.negativity.integration.geysermc.geysermc;
+
+import com.elikill58.negativity.universal.Adapter;
+import com.elikill58.negativity.universal.PluginDependentExtension;
+import com.elikill58.negativity.universal.bedrock.data.BedrockClientDataGetter;
+import com.elikill58.negativity.universal.bedrock.data.BedrockClientDataProvider;
+
+public class GeyserClientDataProvider implements BedrockClientDataProvider, PluginDependentExtension {
+
+	@Override
+	public BedrockClientDataGetter create(Adapter adapter) {
+		try {
+			Class<?> processorClass = getClass().getClassLoader()
+					.loadClass("com.elikill58.negativity.integration.geysermc.geysermc.GeyserClientDataGetter");
+			return (BedrockClientDataGetter) processorClass.getConstructor().newInstance();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public String getPluginId() {
+		return "Geyser-" + Adapter.getAdapter().getPlatformID().getCompleteName();
+	}
+}

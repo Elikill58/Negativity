fb38346925ca61355158107f5da9bbf20b2dc7b7
Elikill58 <arpetzouille@gmail.com>
2022-12-20 20:51:39 +0100

    Move professionalbans package

--- spigot/integrations/professionalbans/src/com/elikill58/negativity/spigot/integration/professionalbans/ProfessionalBansProcessor.java
+++ spigot/integrations/professionalbans/src/com/elikill58/negativity/spigot/integration/professionalbans/ProfessionalBansProcessor.java
@@ -1,4 +1,4 @@
-package com.elikill58.negativity.common.integration.professionalbans;
+package com.elikill58.negativity.spigot.integration.professionalbans;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;

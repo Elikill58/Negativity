e5dab597646161c42be9a24e661b3a43a67035ea
Elikill58 <arpetzouille@gmail.com>
2023-03-21 19:17:37 +0100

    Prevent long time load

--- velocity/src/com/elikill58/negativity/velocity/VelocityNegativity.java
+++ velocity/src/com/elikill58/negativity/velocity/VelocityNegativity.java
@@ -33,23 +33,25 @@ import com.velocitypowered.api.proxy.messages.LegacyChannelIdentifier;
 
 @Plugin(id = "negativity")
 public class VelocityNegativity {
-	
+
 	private static VelocityNegativity instance;
+
 	public static VelocityNegativity getInstance() {
 		return instance;
 	}
+
 	public static final LegacyChannelIdentifier NEGATIVITY_CHANNEL_ID = new LegacyChannelIdentifier(NegativityMessagesManager.CHANNEL_ID);
-	
+
 	private static final String MYSQL_DRIVER_URL = "https://repo1.maven.org/maven2/mysql/mysql-connector-java/8.0.25/mysql-connector-java-8.0.25.jar";
 	private static final String MYSQL_DRIVER_FILE_NAME = "mysql-connector-java-8.0.25.jar";
 	private static final String EXPECTED_MYSQL_DRIVER_HASH_STRING = "F8B9123ACD13058C941AFF25F308C9ED8000BB73";
 	private static final byte[] EXPECTED_MYSQL_DRIVER_HASH = UniversalUtils.stringToHex(EXPECTED_MYSQL_DRIVER_HASH_STRING);
-	
+
 	private final ProxyServer server;
 	private final Logger logger;
 	private final PluginContainer container;
 	private final PluginManager pluginManager;
-	
+
 	@Inject
 	public VelocityNegativity(ProxyServer server, Logger logger, PluginContainer container, PluginManager pluginManager) {
 		instance = this;
@@ -58,19 +60,19 @@ public class VelocityNegativity {
 		this.container = container;
 		this.pluginManager = pluginManager;
 	}
-	
+
 	public ProxyServer getServer() {
 		return server;
 	}
-	
+
 	public Logger getLogger() {
 		return logger;
 	}
-	
+
 	public PluginContainer getContainer() {
 		return container;
 	}
-	
+
 	@Subscribe
 	public void onProxyInitialization(ProxyInitializeEvent event) {
 		getLogger().info("Loading Negativity");
@@ -78,33 +80,33 @@ public class VelocityNegativity {
 		server.getEventManager().register(this, new VelocityListeners());
 		server.getChannelRegistrar().register(NEGATIVITY_CHANNEL_ID);
 		server.getCommandManager().register("vnegativity", new VNegativityCommand());
-		
+
 		Adapter.setAdapter(new VelocityAdapter(this));
 		Negativity.loadNegativity();
-		
+
 		NegativityAccountStorage.setDefaultStorage("database");
-		
+
 		Stats.sendStartupStats(getServer().getBoundAddress().getPort());
 		getLogger().info("Negativity enabled");
 	}
-	
+
 	@Subscribe
 	public void onProxyDisable(ProxyShutdownEvent e) {
 		Negativity.closeNegativity();
 	}
-	
+
 	public final File getDataFolder() {
 		return new File("./plugins/Negativity");
 	}
-	
+
 	private void downloadAndInstallMysqlDriverIfNeeded() {
 		Path mysqlDriverPath = Paths.get("plugins", "Negativity", MYSQL_DRIVER_FILE_NAME).toAbsolutePath();
-		
+
 		if (Files.isDirectory(mysqlDriverPath)) {
 			getLogger().error("Unexpected directory at {}. Can't download and use MYSQL driver.", mysqlDriverPath);
 			return;
 		}
-		
+
 		MessageDigest messageDigest;
 		try {
 			messageDigest = MessageDigest.getInstance("SHA-1");
@@ -112,42 +114,32 @@ public class VelocityNegativity {
 			getLogger().error("Could not find SHA-1 digest algorithm");
 			return;
 		}
-		
+
 		if (Files.exists(mysqlDriverPath)) {
-			try (InputStream inputStream = Files.newInputStream(mysqlDriverPath);
-				 DigestInputStream digestInputStream = new DigestInputStream(inputStream, messageDigest)) {
-				// Completely read file to compute its digest
-				//noinspection StatementWithEmptyBody
-				while (digestInputStream.read() != -1) ;
-				byte[] digest = messageDigest.digest();
-				if (MessageDigest.isEqual(EXPECTED_MYSQL_DRIVER_HASH, digest)) {
-					getLogger().warn("Existing MYSQL driver matches expected checksum. Adding it to classpath.");
-					this.pluginManager.addToClasspath(this, mysqlDriverPath);
-					return;
-				}
-				
-				getLogger().warn("Existing MYSQL driver does not match expected checksum. Downloading it again...");
-			} catch (IOException e) {
-				getLogger().error("Failed to read existing MYSQL driver at {}", mysqlDriverPath, e);
+			try {
+				this.pluginManager.addToClasspath(this, mysqlDriverPath);
+				getLogger().info("Loaded previously downloaded MySQL driver ...");
+				return;
+			} catch (Exception e) {
+				getLogger().error("Failed add existing MySQL driver to classpath: {}", mysqlDriverPath, e);
 			}
 		} else {
 			getLogger().info("Missing MYSQL driver, downloading it...");
 		}
-		
+
 		try {
 			Files.createDirectories(mysqlDriverPath.getParent());
 		} catch (IOException e) {
 			getLogger().error("Failed to create Negativity configuration directory.", e);
 			return;
 		}
-		
+
 		try {
 			URL url = new URL(MYSQL_DRIVER_URL);
 			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 			connection.setUseCaches(true);
 			connection.setRequestProperty("User-Agent", "Negativity Velocity - " + container.getDescription().getVersion());
-			try (InputStream inputStream = connection.getInputStream();
-				 DigestInputStream digestInputStream = new DigestInputStream(inputStream, messageDigest)) {
+			try (InputStream inputStream = connection.getInputStream(); DigestInputStream digestInputStream = new DigestInputStream(inputStream, messageDigest)) {
 				Files.copy(digestInputStream, mysqlDriverPath, StandardCopyOption.REPLACE_EXISTING);
 				byte[] digest = messageDigest.digest();
 				if (MessageDigest.isEqual(EXPECTED_MYSQL_DRIVER_HASH, digest)) {

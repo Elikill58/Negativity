c555cbf27322afc07947ae5e6691a6f18bcdc345
Elikill58 <arpetzouille@gmail.com>
2023-04-03 22:07:58 +0200

    Fix #266

--- common/src/com/elikill58/negativity/common/ConnectionManager.java
+++ common/src/com/elikill58/negativity/common/ConnectionManager.java
@@ -38,30 +38,32 @@ public class ConnectionManager implements Listeners {
 	public void onConnect(PlayerConnectEvent e) {
 		Player p = e.getPlayer();
 		NegativityPlayer np = e.getNegativityPlayer();
-		if(!Adapter.getAdapter().getPlatformID().isProxy())
+		Adapter ada = Adapter.getAdapter();
+		if (!ada.getPlatformID().isProxy())
 			np.delta = np.lastDelta = p.getLocation().clone();
 		np.addInvincibilityTicks(5, "Connection");
-		Adapter.getAdapter().debug(Debug.GENERAL, "Player " + p.getName() + " just connect using protocol " + p.getProtocolVersion() + " (version: " + p.getPlayerVersion().getName() + "). EntityId: " + p.getEntityId());
-		if(UniversalUtils.isMe(p.getUniqueId()))
+		ada.debug(Debug.GENERAL,
+				"Player " + p.getName() + " just connect using protocol " + p.getProtocolVersion() + " (version: " + p.getPlayerVersion().getName() + "). EntityId: " + p.getEntityId());
+		if (UniversalUtils.isMe(p.getUniqueId()))
 			p.sendMessage(ChatColor.GREEN + "Ce serveur utilise Negativity ! Waw :')");
-		
+
 		String ip = p.getIP();
 		List<Ban> banOnIP = BanManager.getActiveBanOnSameIP(ip);
-		if(banOnIP != null && !banOnIP.isEmpty()) {
+		if (banOnIP != null && !banOnIP.isEmpty()) {
 			List<NegativityAccount> accounts = new ArrayList<>();
 			banOnIP.forEach((ban) -> accounts.add(NegativityAccount.get(ban.getPlayerId())));
-			
+
 			long warns = accounts.stream().mapToLong(NegativityAccount::countAllWarns).sum();
-			
+
 			AltAccountBan alt = BanManager.getAltBanFor(banOnIP.size() + 1);
-			if(alt != null && alt.hasCondition(warns, warns)) {
+			if (alt != null && alt.hasCondition(warns, warns)) {
 				String reason = alt.getAlertMessage() == null ? "Alt unauthorized" : alt.getAlertMessage();
 				switch (alt.getAction()) {
 				case ALERT:
 					p.sendMessage(alt.getAlertMessage());
 					break;
 				case ALERT_MOD:
-					Adapter.getAdapter().getOnlinePlayers().stream().filter((mod) -> Perm.hasPerm(mod, Perm.SHOW_ALERT))
+					ada.getOnlinePlayers().stream().filter((mod) -> Perm.hasPerm(mod, Perm.SHOW_ALERT))
 							.forEach((mod) -> mod.sendMessage(alt.getAlertMessage().replaceAll("%name%", p.getName())));
 					break;
 				case BAN:
@@ -69,29 +71,27 @@ public class ConnectionManager implements Listeners {
 					break;
 				case BAN_ALL:
 					long time = alt.isBanDef() ? -1 : alt.getBanTime();
-					for(UUID allPlayers : NegativityAccountStorage.getStorage().getPlayersOnIP(ip)) {
+					for (UUID allPlayers : NegativityAccountStorage.getStorage().getPlayersOnIP(ip)) {
 						BanManager.executeBan(new Ban(allPlayers, reason, "Negativity", SanctionnerType.PLUGIN, time, "Alt", ip, BanStatus.ACTIVE));
 					}
 					break;
 				}
 			}
 		}
-		
+
 		if (Perm.hasPerm(np, Perm.SHOW_REPORT)) {
-			if(ReportCommand.REPORT_LAST.size() > 0) {
+			if (ReportCommand.REPORT_LAST.size() > 0) {
 				for (String msg : ReportCommand.REPORT_LAST)
 					p.sendMessage(msg);
 				ReportCommand.REPORT_LAST.clear();
 			}
-			
+
 			if (Perm.hasPerm(np, Perm.SHOW_ALERT)) {
 				CompletableFuture.runAsync(() -> {
 					SemVer latestVersion = UniversalUtils.getLatestVersionIfNewer();
 					if (latestVersion != null) {
-						Adapter.getAdapter().sendMessageRunnableHover(p,
-							ChatColor.YELLOW + "New version of Negativity available: " + latestVersion.toFormattedString() + ". "
-								+ ChatColor.BOLD + "Download it here.",
-							"Click here", "https://www.spigotmc.org/resources/48399/");
+						ada.sendMessageRunnableHover(p, Messages.getMessage(p, "messages_update.new.message", "%version%", latestVersion.toFormattedString()),
+								Messages.getMessage(p, "messages_update.new.hover"), "https://www.spigotmc.org/resources/86874/");
 					}
 				});
 			}
@@ -101,12 +101,12 @@ public class ConnectionManager implements Listeners {
 	@EventListener
 	public void onLogin(LoginEvent e) {
 		NegativityPlayer.removeFromCache(e.getUUID()); // remove all possible old things
-		if(!BanManager.shouldNegativityHandleBans() || !e.getLoginResult().equals(Result.ALLOWED) || !BanManager.banActive) // already kicked or ban not enabled
+		if (!BanManager.shouldNegativityHandleBans() || !e.getLoginResult().equals(Result.ALLOWED) || !BanManager.banActive) // already kicked or ban not enabled
 			return;
 		UUID playerId = e.getUUID();
 
 		NegativityAccount account = NegativityAccount.get(playerId);
-		
+
 		Adapter ada = Adapter.getAdapter();
 		Ban activeBan = BanManager.getActiveBan(playerId);
 		if (activeBan != null) {
@@ -124,13 +124,13 @@ public class ConnectionManager implements Listeners {
 			ada.getAccountManager().dispose(playerId);
 		}
 	}
-	
+
 	@EventListener
 	public void onTeleport(PlayerTeleportEvent e) {
 		Player p = e.getPlayer();
 		NegativityPlayer.getNegativityPlayer(p).addInvincibilityTicks(3, "Teleportation");
 	}
-	
+
 	@EventListener
 	public void onLeft(PlayerLeaveEvent e) {
 		WebhookManager.getWebhooks().forEach(w -> w.clean(e.getPlayer()));

880e8f2aa9ce63e985539d95c554e213774cff39
Elikill58 <arpetzouille@gmail.com>
2024-09-26 17:54:16 +0200

    Add option for complex alerts

--- common/src/com/elikill58/negativity/universal/Negativity.java
+++ common/src/com/elikill58/negativity/universal/Negativity.java
@@ -58,24 +58,21 @@ public class Negativity {
 	public static boolean log = false, log_console = false, hasBypass = false, tpsDrop = false;
 	public static boolean disabledJava, disabledBedrock = false;
 	public static double tpsDropStop = 19;
-	
+
 	private static ScheduledTask actualizeInvTimer, analyzePacketTimer;
 
 	/**
-	 * Try to alert moderator.
-	 * It will not check everything, but there is some verification which are made such as:
-	 * - if the cheat is active
-	 * - the reliability amount
-	 * - Detection disabled on verification
-	 * - Bypass with permissions
-	 * - ...
+	 * Try to alert moderator. It will not check everything, but there is some
+	 * verification which are made such as: - if the cheat is active - the
+	 * reliability amount - Detection disabled on verification - Bypass with
+	 * permissions - ...
 	 * 
-	 * @param type the report type
-	 * @param p the player which have to be reported
-	 * @param c the cheat that just detect the player
+	 * @param type        the report type
+	 * @param p           the player which have to be reported
+	 * @param c           the cheat that just detect the player
 	 * @param reliability the reliability of the cheat
-	 * @param checkName the name of the check
-	 * @param proof the proof which will be on proof file
+	 * @param checkName   the name of the check
+	 * @param proof       the proof which will be on proof file
 	 * @return true if the player have to be set back
 	 */
 	public static boolean alertMod(ReportType type, Player p, Cheat c, int reliability, String checkName, String proof) {
@@ -83,61 +80,52 @@ public class Negativity {
 	}
 
 	/**
-	 * Try to alert moderator.
-	 * It will not check everything, but there is some verification which are made such as:
-	 * - if the cheat is active
-	 * - the reliability amount
-	 * - Detection disabled on verification
-	 * - Bypass with permissions
-	 * - ...
+	 * Try to alert moderator. It will not check everything, but there is some
+	 * verification which are made such as: - if the cheat is active - the
+	 * reliability amount - Detection disabled on verification - Bypass with
+	 * permissions - ...
 	 * 
-	 * @param type the report type
-	 * @param p the player which have to be reported
-	 * @param c the cheat that just detect the player
+	 * @param type        the report type
+	 * @param p           the player which have to be reported
+	 * @param c           the cheat that just detect the player
 	 * @param reliability the reliability of the cheat
-	 * @param checkName the name of the check
-	 * @param proof the proof which will be on proof file
-	 * @param hover the cheatHover see in alert
+	 * @param checkName   the name of the check
+	 * @param proof       the proof which will be on proof file
+	 * @param hover       the cheatHover see in alert
 	 * @return true if the player have to be set back
 	 */
-	public static boolean alertMod(ReportType type, Player p, Cheat c, int reliability, String checkName, String proof,
-			CheatHover hover) {
+	public static boolean alertMod(ReportType type, Player p, Cheat c, int reliability, String checkName, String proof, CheatHover hover) {
 		return alertMod(type, p, c, reliability, checkName, proof, hover, 1);
 	}
 
 	/**
-	 * Try to alert moderator.
-	 * It will not check everything, but there is some verification which are made such as:
-	 * - if the cheat is active
-	 * - the reliability amount
-	 * - Detection disabled on verification
-	 * - Bypass with permissions
-	 * - ...
+	 * Try to alert moderator. It will not check everything, but there is some
+	 * verification which are made such as: - if the cheat is active - the
+	 * reliability amount - Detection disabled on verification - Bypass with
+	 * permissions - ...
 	 * 
-	 * @param type the report type
-	 * @param p the player which have to be reported
-	 * @param c the cheat that just detect the player
+	 * @param type        the report type
+	 * @param p           the player which have to be reported
+	 * @param c           the cheat that just detect the player
 	 * @param reliability the reliability of the cheat
-	 * @param checkName the name of the check
-	 * @param proof the proof which will be on proof file
-	 * @param hover the cheatHover see in alert
-	 * @param amount the amount of alert
+	 * @param checkName   the name of the check
+	 * @param proof       the proof which will be on proof file
+	 * @param hover       the cheatHover see in alert
+	 * @param amount      the amount of alert
 	 * @return true if the player have to be set back
 	 */
-	public static boolean alertMod(ReportType type, Player p, Cheat c, int reliability, String checkName, String proof,
-			CheatHover hover, long amount) {
-		if(!c.isActive() || reliability < 55 || tpsDrop || amount <= 0)
+	public static boolean alertMod(ReportType type, Player p, Cheat c, int reliability, String checkName, String proof, CheatHover hover, long amount) {
+		if (!c.isActive() || reliability < 55 || tpsDrop || amount <= 0)
 			return false;
 		NegativityPlayer np = NegativityPlayer.getNegativityPlayer(p);
-		if(VerificationManager.isDisablingAlertOnVerif() && hasVerifications(p.getUniqueId()))
+		if (VerificationManager.isDisablingAlertOnVerif() && hasVerifications(p.getUniqueId()))
 			return false;
 		int ping = p.getPing();
 		if (ping < 0)
 			return false;
-		
+
 		EventManager.callEvent(new PlayerCheatEvent(p, c, reliability));
-		if (hasBypass && (Perm.hasPerm(NegativityPlayer.getNegativityPlayer(p), "bypass." + c.getKey().getLowerKey())
-				|| Perm.hasPerm(NegativityPlayer.getNegativityPlayer(p), Perm.BYPASS_ALL))) {
+		if (hasBypass && (Perm.hasPerm(NegativityPlayer.getNegativityPlayer(p), "bypass." + c.getKey().getLowerKey()) || Perm.hasPerm(NegativityPlayer.getNegativityPlayer(p), Perm.BYPASS_ALL))) {
 			PlayerCheatBypassEvent bypassEvent = new PlayerCheatBypassEvent(p, c, reliability);
 			EventManager.callEvent(bypassEvent);
 			if (!bypassEvent.isCancelled())
@@ -148,15 +136,15 @@ public class Negativity {
 		if (alert.isCancelled() || !alert.isAlert())
 			return false;
 		long oldWarn = np.addWarn(c, reliability, amount);
-		if(oldWarn == -1) // no warn added
+		if (oldWarn == -1) // no warn added
 			return false;
-		if(log && !type.equals(ReportType.INFO))
+		if (log && !type.equals(ReportType.INFO))
 			NegativityProofStorage.getStorage().saveProof(new Proof(np, type, c.getKey(), checkName, ping, amount, reliability, proof));
 		if (c.allowKick() && ((long) (oldWarn / c.getAlertToKick())) < ((long) (np.getWarn(c) / c.getAlertToKick()))) { // if reach new alert state
 			PlayerCheatKickEvent kick = new PlayerCheatKickEvent(p, c, reliability);
 			EventManager.callEvent(kick);
 			if (!kick.isCancelled()) {
-				if(Adapter.getAdapter().getConfig().getBoolean("log_alert_with_kick", false)) { // if should log
+				if (Adapter.getAdapter().getConfig().getBoolean("log_alert_with_kick", false)) { // if should log
 					manageAlertCommand(np, type, p, c, reliability);
 					sendAlertMessage(np, alert);
 					// don't run set back options because player will be offline
@@ -172,14 +160,14 @@ public class Negativity {
 			}
 		}
 		Stats.updateCheat(c, (int) amount);
-		if(BanManager.isBanned(np.getUUID()))
+		if (BanManager.isBanned(np.getUUID()))
 			return false;
 
 		if (BanManager.autoBan && BanUtils.banIfNeeded(np, c, reliability, oldWarn).isSuccess())
 			return false;
 		manageAlertCommand(np, type, p, c, reliability);
 		AlertSender.getAlertShower().alert(np, alert);
-		if(c.isSetBack())
+		if (c.isSetBack())
 			c.performSetBack(p);
 		return true;
 	}
@@ -187,56 +175,55 @@ public class Negativity {
 	/**
 	 * Run all command set in config is it's active and the reliability is enough
 	 * 
-	 * @param np the negativity player which create alert
-	 * @param type the type of the alert
-	 * @param p the player which create alert
-	 * @param c the cheat which detect player
+	 * @param np          the negativity player which create alert
+	 * @param type        the type of the alert
+	 * @param p           the player which create alert
+	 * @param c           the cheat which detect player
 	 * @param reliability the reliability of detection
 	 */
 	private static void manageAlertCommand(NegativityPlayer np, ReportType type, Player p, Cheat c, int reliability) {
 		Configuration conf = Adapter.getAdapter().getConfig().getSection("alert.command");
-		if(conf == null || !conf.getBoolean("active") || conf.getInt("reliability_need") > reliability)
+		if (conf == null || !conf.getBoolean("active") || conf.getInt("reliability_need") > reliability)
 			return;
 		int cooldown = conf.getInt("cooldown", 0);
-		if(cooldown > 0) {
-			if(np.longs.get(CheatKeys.ALL, "alert-cmd-cooldown", 0l) > System.currentTimeMillis())
+		if (cooldown > 0) {
+			if (np.longs.get(CheatKeys.ALL, "alert-cmd-cooldown", 0l) > System.currentTimeMillis())
 				return; // has cooldown
 			np.longs.set(CheatKeys.ALL, "alert-cmd-cooldown", System.currentTimeMillis() + cooldown);
 		}
-		for(String s : conf.getStringList("run")) {
-			Adapter.getAdapter().runConsoleCommand(UniversalUtils.replacePlaceholders(s, "%name%",
-					p.getName(), "%uuid%", p.getUniqueId().toString(), "%cheat_key%", c.getKey().getLowerKey(), "%cheat_name%",
-					c.getName(), "%reliability%", reliability, "%report_type%", type.name(), "%warn%", np.getWarn(c)));
+		for (String s : conf.getStringList("run")) {
+			Adapter.getAdapter().runConsoleCommand(UniversalUtils.replacePlaceholders(s, "%name%", p.getName(), "%uuid%", p.getUniqueId().toString(), "%cheat_key%", c.getKey().getLowerKey(),
+					"%cheat_name%", c.getName(), "%reliability%", reliability, "%report_type%", type.name(), "%warn%", np.getWarn(c)));
 		}
 	}
 
 	/**
-	 * Send alert message to all player which can receive them
-	 * If any, the message will keep in cache and showed when someone can see them
-	 * If a lot of one, all message are compiled into one (and amount or are additionned)
+	 * Send alert message to all player which can receive them If any, the message
+	 * will keep in cache and showed when someone can see them If a lot of one, all
+	 * message are compiled into one (and amount or are additionned)
 	 * 
-	 * @param np the negativity player which create alert
+	 * @param np    the negativity player which create alert
 	 * @param alert a compiled alert
 	 */
 	public static void sendAlertMessage(NegativityPlayer np, PlayerCheatAlertEvent alert) {
 		Cheat c = alert.getCheat();
 		int reliability = alert.getReliability();
-		if(reliability == 0) {// alert already sent
+		if (reliability == 0) {// alert already sent
 			np.alertNotShowed.remove(c.getKey());
 			return;
 		}
 		Adapter ada = Adapter.getAdapter();
 		Player p = alert.getPlayer();
 		int ping = alert.getPing();
-		if(alert.getNbAlertConsole() > 0 && log_console) {
+		if (alert.getNbAlertConsole() > 0 && log_console) {
 			Location location = p.getLocation();
 			String sLoc = "[" + location.getWorld().getName() + ": " + location.getBlockX() + "," + location.getBlockY() + "," + location.getBlockZ() + "]";
-			ada.getLogger().info("New " + alert.getReportType().getName() + " for " + p.getName() + " (" + ping + " ms, UUID: "
-						+ p.getUniqueId() + ") seem to use " + c.getName() + " "
-						+ (alert.getNbAlertConsole() > 1 ? alert.getNbAlertConsole() + " times " : "") + "Reliability: " + reliability + " " + sLoc);
+			ada.getLogger().info("New " + alert.getReportType().getName() + " for " + p.getName() + " (" + ping + " ms, UUID: " + p.getUniqueId() + ") seem to use " + c.getName() + " "
+					+ (alert.getNbAlertConsole() > 1 ? alert.getNbAlertConsole() + " times " : "") + "Reliability: " + reliability + " " + sLoc);
 		}
-		if(!alert.getReportType().equals(ReportType.INFO))
-			WebhookManager.addToQueue(new AlertWebhookMessage(WebhookMessageType.ALERT, p, "Negativity", System.currentTimeMillis(), alert.getNbAlert() == 0 ? 1 : alert.getNbAlert(), alert.getReliability(), alert.getCheat()));
+		if (!alert.getReportType().equals(ReportType.INFO))
+			WebhookManager.addToQueue(new AlertWebhookMessage(WebhookMessageType.ALERT, p, "Negativity", System.currentTimeMillis(), alert.getNbAlert() == 0 ? 1 : alert.getNbAlert(),
+					alert.getReliability(), alert.getCheat()));
 		CheatHover hoverMsg = alert.getHover();
 		if (ProxyCompanionManager.isIntegrationEnabled()) {
 			p.sendPluginMessage(NegativityMessagesManager.CHANNEL_ID, new AlertMessage(p.getUniqueId(), c.getName(), reliability, ping, hoverMsg, alert.getNbAlert()));
@@ -251,24 +238,30 @@ public class Negativity {
 				if (permissionEvent.isCancelled() || !npMod.getAccount().isShowAlert())
 					continue;
 				if (permissionEvent.hasBasicPerm()) {
-					ada.sendMessageRunnableHover(pl, Messages.getMessage(pl, alert.getAlertMessageKey(), "%name%", p.getName(), "%cheat%", c.getName(),
-									"%reliability%", reliability, "%nb%", alert.getNbAlert()),
-							Messages.getMessage(pl, "negativity.alert_hover", "%reliability%", reliability, "%ping%", ping)
-							+ "\n" + ChatColor.GRAY + "Check: " + alert.getCheckName() + (hoverMsg == null ? "" : "\n\n" + hoverMsg.compile(npMod)), "/negativity " + p.getName());
+					if (ada.getConfig().getBoolean("alert.complex", true)) {
+						ada.sendMessageRunnableHover(pl,
+								Messages.getMessage(pl, alert.getAlertMessageKey(), "%name%", p.getName(), "%cheat%", c.getName(), "%reliability%", reliability, "%nb%", alert.getNbAlert()),
+								Messages.getMessage(pl, "negativity.alert_hover", "%reliability%", reliability, "%ping%", ping) + "\n" + ChatColor.GRAY + "Check: " + alert.getCheckName()
+										+ (hoverMsg == null ? "" : "\n\n" + hoverMsg.compile(npMod)),
+								"/negativity " + p.getName());
+					} else {
+						Messages.sendMessage(pl, alert.getAlertMessageKey(), "%name%", p.getName(), "%cheat%", c.getName(), "%reliability%", reliability, "%nb%", alert.getNbAlert());
+					}
 					hasPermPeople = true;
 				}
 			}
-			if(hasPermPeople) {
+			if (hasPermPeople) {
 				np.alertNotShowed.remove(c.getKey());
 			}
 		}
 	}
 
 	/**
-	 * If there is a proxy, and we don't are on proxy platform, the message is sent to them.
+	 * If there is a proxy, and we don't are on proxy platform, the message is sent
+	 * to them.
 	 * 
 	 * @param reporter the player which report the other player
-	 * @param reason the reason of the report
+	 * @param reason   the reason of the report
 	 * @param reported the name of the reported player
 	 */
 	public static void sendReportMessage(Player reporter, String reason, String reported) {
@@ -280,19 +273,18 @@ public class Negativity {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private static final Set<String> integratedPlugins = Collections.synchronizedSet(new HashSet<>());
-	
+
 	/**
-	 * Load all Negativity's class and content.
-	 * Must to be run after setting adapter
+	 * Load all Negativity's class and content. Must to be run after setting adapter
 	 */
 	public static void loadNegativity() {
 		Adapter ada = Adapter.getAdapter();
 		ada.getLogger().info("Running with Java " + System.getProperty("java.version"));
 		ada.getLogger().info("Thanks for buying Negativity <3");
 		ada.reloadConfig();
-		
+
 		integratedPlugins.clear();
 
 		DefaultConfigValue.init();
@@ -307,7 +299,7 @@ public class Negativity {
 		BedrockPlayerManager.init();
 		Special.loadSpecial();
 		Configuration config = ada.getConfig();
-		if(!ada.getPlatformID().isProxy()) {
+		if (!ada.getPlatformID().isProxy()) {
 			Cheat.loadCheat();
 			BypassManager.loadBypass();
 			PlayerModificationsManager.init();
@@ -318,18 +310,18 @@ public class Negativity {
 				NegativityPlayer.getNegativityPlayer(p).setClientName(new String(msg).substring(1));
 			});
 			AlertSender.initAlertShower(ada);
-			if(actualizeInvTimer != null)
+			if (actualizeInvTimer != null)
 				actualizeInvTimer.cancel();
 			actualizeInvTimer = ada.getScheduler().runRepeating(new ActualizeInvTimer(), 10, 10);
-			if(analyzePacketTimer != null)
+			if (analyzePacketTimer != null)
 				analyzePacketTimer.cancel();
 			analyzePacketTimer = ada.getScheduler().runRepeating(new AnalyzePacketTimer(), 20, 20);
-			if(config.getBoolean("use-proxy-force", false)) {
+			if (config.getBoolean("use-proxy-force", false)) {
 				ProxyCompanionManager.forceCompanion();
 				ada.getLogger().info("Force proxy used without sending searching message.");
 			} else {
 				List<Player> players = Adapter.getAdapter().getOnlinePlayers();
-				if(!players.isEmpty()) {
+				if (!players.isEmpty()) {
 					ProxyCompanionManager.sendProxyPing(players.get(0));
 				}
 			}
@@ -340,7 +332,7 @@ public class Negativity {
 		disabledBedrock = config.getBoolean("config_all.bedrock.disabled", false);
 		disabledJava = config.getBoolean("config_all.java.disabled", false);
 		tpsDropStop = config.getDouble("tps_alert_stop", 19);
-		
+
 		if (!integratedPlugins.isEmpty()) {
 			ada.getLogger().info("Loaded support for " + String.join(", ", integratedPlugins) + ".");
 		}
@@ -353,7 +345,7 @@ public class Negativity {
 		});
 		ada.getLogger().info("Negativity " + ada.getPluginVersion() + " loaded for server version " + ada.getServerVersion().getName());
 	}
-	
+
 	public static <T> void loadExtensions(Class<T> extensionClass, Predicate<T> extensionConsumer) {
 		Adapter adapter = Adapter.getAdapter();
 		// First load extensions from negativity
@@ -364,15 +356,14 @@ public class Negativity {
 			safelyLoadExtensions(extensionClass, pluginClassLoader, extensionConsumer, adapter);
 		}
 	}
-	
+
 	private static <T> void safelyLoadExtensions(Class<T> extensionClass, ClassLoader classLoader, Predicate<T> extensionConsumer, Adapter adapter) {
 		for (T extension : ServiceLoader.load(extensionClass, classLoader)) {
 			try {
-				if (extension instanceof PlatformDependentExtension
-					&& !((PlatformDependentExtension) extension).getPlatforms().contains(adapter.getPlatformID())) {
+				if (extension instanceof PlatformDependentExtension && !((PlatformDependentExtension) extension).getPlatforms().contains(adapter.getPlatformID())) {
 					continue;
 				}
-				
+
 				String dependencyPluginId = null;
 				if (extension instanceof PluginDependentExtension) {
 					PluginDependentExtension depExt = (PluginDependentExtension) extension;
@@ -381,7 +372,7 @@ public class Negativity {
 						continue;
 					}
 				}
-				
+
 				if (extensionConsumer.test(extension) && dependencyPluginId != null) {
 					integratedPlugins.add(dependencyPluginId);
 				}
@@ -391,11 +382,11 @@ public class Negativity {
 			}
 		}
 	}
-	
+
 	public static void closeNegativity() {
-		if(actualizeInvTimer != null)
+		if (actualizeInvTimer != null)
 			actualizeInvTimer.cancel();
-		if(analyzePacketTimer != null)
+		if (analyzePacketTimer != null)
 			analyzePacketTimer.cancel();
 		NegativityPlayer.getAllNegativityPlayers().forEach(NegativityPlayer::destroy);
 	}
